c828b783117a97c0aebed4287b53bed1552077a7
Joe Burmeister <joe.burmeister@devtank.co.uk>
2018-08-10 10:24:11 +0100

    RTOS kind of working, but vTaskDelay isn't.

--- main.c
+++ main.c
@@ -7,12 +7,20 @@
 #include <libopencm3/stm32/gpio.h>
 
 
+static void flash_led(unsigned delay) {
+    for(unsigned n = 0; n < delay; n++)
+        asm("nop");
+    gpio_toggle(GPIOA, GPIO5);
+}
+
+
 static void
 task1(void *args __attribute((unused))) {
 
 	for (;;) {
-		gpio_toggle(GPIOA, GPIO5);
-		vTaskDelay(pdMS_TO_TICKS(500));
+		flash_led(1000000);
+		vTaskDelay(5000000);
+		flash_led(1000000);
 	}
 }
 
@@ -21,13 +29,6 @@ int main(void)
     rcc_clock_setup_in_hse_8mhz_out_48mhz();
     rcc_periph_clock_enable(RCC_GPIOA);
     gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
-
-    for(unsigned n = 0; n < 10; n++)
-    {
-        for(unsigned n = 0; n < 10000000; n++)
-            asm("nop");
-        gpio_toggle(GPIOA, GPIO5);
-    }
 
     xTaskCreate(task1,"LED",100,NULL,configMAX_PRIORITIES-1,NULL);
     vTaskStartScheduler();
