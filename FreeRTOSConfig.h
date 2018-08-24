34680bcf442b4ab4cec6a70fb235e5d0f165326a
Joe Burmeister <joe.burmeister@devtank.co.uk>
2018-08-24 21:10:38 +0100

    Clearer FreeRTOSConfig.h for anymore following.

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -80,56 +80,57 @@
  * application requirements.
  *
  * THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
- * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE. 
+ * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.
  *
  * See http://www.freertos.org/a00110.html.
  *----------------------------------------------------------*/
 
-#define configUSE_PREEMPTION		1
-#define configUSE_IDLE_HOOK		0
-#define configUSE_TICK_HOOK		0
-#define configCPU_CLOCK_HZ		( ( unsigned long ) rcc_ahb_frequency )	
-#define configSYSTICK_CLOCK_HZ 		( ( portTickType ) 1000 )
-#define configTICK_RATE_HZ		( ( TickType_t ) 1000 )
-#define configMAX_PRIORITIES		( 5 )
-#define configMINIMAL_STACK_SIZE	( ( unsigned short ) 128 )
-#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 10 * 1024 ) )
-#define configMAX_TASK_NAME_LEN		( 16 )
-#define configUSE_TRACE_FACILITY	0
-#define configUSE_16_BIT_TICKS		0
-#define configIDLE_SHOULD_YIELD		1
-#define configUSE_MUTEXES		1
+#define configUSE_PREEMPTION        1
+#define configUSE_IDLE_HOOK     0
+#define configUSE_TICK_HOOK     0
+#define configCPU_CLOCK_HZ      ( ( unsigned long ) rcc_ahb_frequency )
+#define configSYSTICK_CLOCK_HZ      ( ( portTickType ) 1000 )
+#define configTICK_RATE_HZ      ( ( TickType_t ) 1000 )
+#define configMAX_PRIORITIES        ( 5 )
+#define configMINIMAL_STACK_SIZE    ( ( unsigned short ) 128 )
+#define configTOTAL_HEAP_SIZE       ( ( size_t ) ( 10 * 1024 ) )
+#define configMAX_TASK_NAME_LEN     ( 16 )
+#define configUSE_TRACE_FACILITY    0
+#define configUSE_16_BIT_TICKS      0
+#define configIDLE_SHOULD_YIELD     1
+#define configUSE_MUTEXES       1
 
-#define configCHECK_FOR_STACK_OVERFLOW	1
+#define configCHECK_FOR_STACK_OVERFLOW  1
 
 /* Co-routine definitions. */
-#define configUSE_CO_ROUTINES 		0
+#define configUSE_CO_ROUTINES       0
 #define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
-#define INCLUDE_vTaskPrioritySet	0
-#define INCLUDE_uxTaskPriorityGet	0
-#define INCLUDE_vTaskDelete		0
-#define INCLUDE_vTaskCleanUpResources	0
-#define INCLUDE_vTaskSuspend		0
-#define INCLUDE_vTaskDelayUntil		0
-#define INCLUDE_vTaskDelay		1
+#define INCLUDE_vTaskPrioritySet    0
+#define INCLUDE_uxTaskPriorityGet   0
+#define INCLUDE_vTaskDelete     0
+#define INCLUDE_vTaskCleanUpResources   0
+#define INCLUDE_vTaskSuspend        0
+#define INCLUDE_vTaskDelayUntil     0
+#define INCLUDE_vTaskDelay      1
 
-/* Use the system definition, if there is one */
-#ifdef __NVIC_PRIO_BITS
-	#define configPRIO_BITS       __NVIC_PRIO_BITS
-#else
-	#define configPRIO_BITS       5        /* 32 priority levels */
-#endif
+/*
 
-// CM0 has 2 bits for priority as for implementation
-// 0 is highest - 3 is lowest
-// assign priority 3 for the kernel
-// reserve priority 2 for ISRs within the kernel (allowed to make API calls)
-// reserve priority 0 and 1 for non-OS ISRs
-#define configKERNEL_INTERRUPT_PRIORITY			3 //  (3 << (8 - __NVIC_PRIO_BITS))
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY	2 // (2 << (8 - __NVIC_PRIO_BITS))
+CM0 has 2 bits for priority  lowest number is highest priority
+configKERNEL_INTERRUPT_PRIORITY should be set to the lowest priority.
+configMAX_SYSCALL_INTERRUPT_PRIORITY sets the highest interrupt priority
+  from which interrupt safe FreeRTOS API functions can be called.
+
+ 0 = Highest :
+ 1 =           configMAX_SYSCALL_INTERRUPT_PRIORITY
+ 2 =
+ 3 = Lowest :  configKERNEL_INTERRUPT_PRIORITY
+
+*/
+#define configKERNEL_INTERRUPT_PRIORITY         3
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY    1
 
 #endif /* FREERTOS_CONFIG_H */
