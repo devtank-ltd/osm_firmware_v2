b49e4bfb991cbd427b6e631614b7dfa56e2cc426
Joe Burmeister <joe.burmeister@devtank.co.uk>
2019-01-11 10:19:35 +0000

    Try different way of combing FreeRTOS and libopencm3.

--- main.c
+++ main.c
@@ -6,18 +6,12 @@
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/cm3/nvic.h>
-#include <libopencm3/cm3/systick.h>
 
 #include "cmd.h"
 #include "log.h"
 #include "usb.h"
 #include "uarts.h"
 
-
-
-extern void xPortPendSVHandler( void ) __attribute__ (( naked ));
-extern void xPortSysTickHandler( void );
-extern void vPortSVCHandler( void ) __attribute__ (( naked ));
 
 
 void vApplicationStackOverflowHook(xTaskHandle *pxTask,
@@ -37,31 +31,10 @@ void hard_fault_handler(void)
 }
 
 
-void pend_sv_handler(void)
-{
-    xPortPendSVHandler();
-}
-
-
-void sys_tick_handler(void)
-{
-    xPortSysTickHandler();
-}
-
-
-void sv_call_handler(void)
-{
-    vPortSVCHandler();
-}
-
-
 int main(void)
 {
     rcc_clock_setup_pll(&rcc_hse8mhz_configs[RCC_CLOCK_HSE8_72MHZ]);
     uarts_setup();
-
-    systick_interrupt_enable();
-    systick_counter_enable();
 
     platform_raw_msg("----start----");
 
