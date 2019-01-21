2b55587041d003d538384e3acd3bf2ac1ef8f810
Joe Burmeister <joe.burmeister@devtank.co.uk>
2019-01-21 09:47:39 +0000

    Break out binding manual test to own file.

--- binding.py
+++ binding.py
@@ -1,4 +1,3 @@
-#! /usr/bin/python3
 import os
 import sys
 import time
@@ -154,30 +153,3 @@ class io_board_py_t(object):
         if _DEBUG:
             print(">>", cmd)
         return self.read_response()
-
-
-devpath = None
-
-for p in serial.tools.list_ports.comports():
-    if p.product == "STM32 STLink":
-        devpath = p.device
-
-if devpath:
-    print("Found device :", devpath)
-else:
-    print("Not STM32 STLink found.")
-    sys.exit(-1)
-
-comm = io_board_py_t(devpath)
-
-for pps in comm.pps:
-    print("PPS", pps.index, pps.value)
-
-for gpio_in in comm.inputs:
-    print("Input", gpio_in.index, gpio_in.value)
-
-for gpio_out in comm.outputs:
-    print("Output", gpio_out.index, gpio_out.value)
-
-for adc in comm.adcs:
-    print("ADC", adc.index, adc.min_value, adc.max_value, adc.avg_value)
---
+++ binding_test.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+import binding
+import serial
+
+devpath = None
+
+for p in serial.tools.list_ports.comports():
+    if p.product == "STM32 STLink":
+        devpath = p.device
+
+if devpath:
+    print("Found device :", devpath)
+else:
+    print("Not STM32 STLink found.")
+    sys.exit(-1)
+
+comm = binding.io_board_py_t(devpath)
+
+for pps in comm.pps:
+    print("PPS", pps.index, pps.value)
+
+for gpio_in in comm.inputs:
+    print("Input", gpio_in.index, gpio_in.value)
+
+for gpio_out in comm.outputs:
+    print("Output", gpio_out.index, gpio_out.value)
+
+for adc in comm.adcs:
+    print("ADC", adc.index, adc.min_value, adc.max_value, adc.avg_value)
