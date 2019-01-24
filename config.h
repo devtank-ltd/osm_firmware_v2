ec6dfa72536873d0d5b6068d086d0a15838360e9
Joe Burmeister <joe.burmeister@devtank.co.uk>
2019-01-24 10:20:07 +0000

    Update interrupt priorities.

--- config.h
+++ config.h
@@ -10,11 +10,11 @@
 #define PRINTF_FMT_CHECK(_fmt_arg, _el_arg)
 #endif
 
-#define UART1_PRIORITY 1
+#define UART1_PRIORITY 2
 #define UART2_PRIORITY 2
-#define UART3_PRIORITY 3
-#define UART4_PRIORITY 4
-#define USB_PRIORITY   5
+#define UART3_PRIORITY 2
+#define UART4_PRIORITY 1
+#define USB_PRIORITY   2
 
 
 #define ARRAY_SIZE(_a) (sizeof(_a)/sizeof(_a[0]))
