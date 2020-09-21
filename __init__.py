099c33b80e98f18b7fa387699c2c22d24eaa03cd
Joe Burmeister <joe.burmeister@devtank.co.uk>
2020-09-21 14:12:16 +0100

    Expose binding debug functions for importer.

--- __init__.py
+++ __init__.py
@@ -20,5 +20,7 @@ if sys.version_info.major == 3:
     # @brief Import all variables in these modules into other namespaces
     #
     from .binding import io_board_py_t, uart_t, gpio_t, adc_t, output_t, input_t, pps_t
+    from .binding import debug_print as io_debug_print, set_debug_print as io_set_debug_print, get_debug_print as io_get_debug_print
 else:
     from binding import io_board_py_t, uart_t, gpio_t, adc_t, output_t, input_t, pps_t
+    from binding import debug_print as io_debug_print, set_debug_print as io_set_debug_print, get_debug_print as io_get_debug_print
