2db332f7800c0376f6f1b9e8aedb465374f877f9
Joe Burmeister <joe.burmeister@devtank.co.uk>
2021-12-17 19:49:02 +0000

    Add way of programming and keep config.

--- scripts/program.sh
+++ scripts/program.sh
@@ -2,4 +2,9 @@
 
 . "$(dirname "$0")"/boot_mode.sh
 
-stm32flash -b 115200 -i $gpio_str -w $1 /dev/$dev
+if [[ -n "$KEEPCONFIG" ]]
+then
+  arg="-e 255"
+fi
+
+stm32flash -b 115200 -i $gpio_str $arg -w $1 /dev/$dev
