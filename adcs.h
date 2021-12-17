f06a8a0cf70c0c363c7516fe329ca3632272f925
Harry Geyer <harry.geyer@devtank.co.uk>
2021-12-17 10:30:18 +0000

    Removed old functionless prototypes from the adc header.

--- adcs.h
+++ adcs.h
@@ -1,21 +1,7 @@
 #ifndef __ADCS__
 #define __ADCS__
 
-
-extern char adc_temp_buffer[24];
-
 extern void     adcs_init(void);
-
-extern void     adcs_do_samples(void);
-extern void     adcs_collect_boardary(void);
-
-extern unsigned adcs_get_count(void);
-
-extern unsigned adcs_get_last(unsigned adc);
-extern unsigned adcs_get_tick(unsigned adc);
-
-extern void     adcs_adc_log(unsigned adc);
-extern void     adcs_log(void);
 
 extern bool adcs_set_midpoint(uint16_t new_midpoint);
 extern void adcs_calibrate_current_clamp(void);
