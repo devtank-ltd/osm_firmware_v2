c329c1544ba155f47c919a28c670b7aec4c1a47f
Joe Burmeister <joe.burmeister@devtank.co.uk>
2021-12-19 15:29:51 +0000

    Get ready for bootloader pinmap.

---
+++ base_types.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <libopencm3/stm32/rcc.h>
+#include <stdint.h>
+
+typedef struct
+{
+    uint32_t port;
+    uint32_t pins;
+} port_n_pins_t;
+
+#define PORT_TO_RCC(_port_)   (RCC_GPIOA + ((_port_ - GPIO_PORT_A_BASE) / 0x400))
+
+/*subset of usb_cdc_line_coding_bParityType*/
+typedef enum
+{
+    uart_parity_none = 0,
+    uart_parity_odd  = 1,
+    uart_parity_even = 2,
+} uart_parity_t;
+
+/*matches usb_cdc_line_coding_bCharFormat*/
+typedef enum
+{
+    uart_stop_bits_1   = 0,
+    uart_stop_bits_1_5 = 1,
+    uart_stop_bits_2   = 2,
+} uart_stop_bits_t;
+
+typedef struct
+{
+    uint32_t              usart;
+    enum rcc_periph_clken uart_clk;
+    uint32_t              baud;
+    uint8_t               databits:4;
+    uint8_t               parity:2 /*uart_parity_t*/;
+    uint8_t               stop:2 /*uart_stop_bits_t*/;
+    uint32_t              gpioport;
+    uint16_t              pins;
+    uint8_t               alt_func_num;
+    uint8_t               irqn;
+    uint32_t              dma_addr;
+    uint32_t              dma_unit;
+    uint32_t              dma_rcc;
+    uint8_t               dma_irqn;
+    uint8_t               dma_channel;
+    uint8_t               priority;
+    uint8_t               enabled;
+} uart_channel_t;
