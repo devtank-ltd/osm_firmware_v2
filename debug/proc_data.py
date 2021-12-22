e808bfe99c786795b8753b1246fc48c730954c51
Harry Geyer <harry.geyer@devtank.co.uk>
2021-12-22 17:50:45 +0000

    Included a python script and real data scraped with gdb to graph and use as template.

---
+++ debug/proc_data.py
@@ -0,0 +1,58 @@
+#! /usr/bin/python3
+
+from scipy.signal import find_peaks
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+
+all_data = []
+
+with open("data.dat", "r") as f:
+    csvf = csv.reader(f)
+    for line in csvf:
+        for datum in line:
+            if datum != '':
+                all_data.append(int(datum))
+
+all_data_reordered = [[], [], [], []]
+
+for i in range(0, len(all_data)):
+    all_data_reordered[i%4].append(all_data[i])
+
+cc_data = all_data_reordered[0]
+
+peak_index,_ = find_peaks(-np.array(cc_data), height=-4095)
+
+peak_vals = []
+for index in peak_index:
+    peak_vals.append(cc_data[index])
+
+# plt.plot(all_data_reordered[1])
+# plt.plot(all_data_reordered[2])
+# plt.plot(all_data_reordered[3])
+
+plt.plot(peak_index, peak_vals)
+plt.plot(cc_data)
+
+midpoint = 2048
+
+max_ = np.max(cc_data)
+min_ = np.min(cc_data)
+rms = midpoint - np.sqrt(np.mean(np.square(np.array(cc_data) - midpoint)))
+rms_2 = np.sqrt(np.mean(np.square(np.array(cc_data))))
+pk = midpoint -np.mean(np.array(peak_vals) - midpoint)
+rms_f = midpoint - (pk-midpoint) * (2 ** (-0.5))
+
+plt.plot(np.full(shape=len(cc_data), fill_value=midpoint, dtype=np.int), "--", color="gray")
+plt.plot(np.full(shape=len(cc_data), fill_value=rms, dtype=np.int), "--", color="red")
+plt.plot(np.full(shape=len(cc_data), fill_value=rms_f, dtype=np.int), "--", color="blue")
+
+print("max = %u"% max_)
+print("min = %u"% min_)
+print("rms = %u"% rms)
+print("rms_2 = %u"% rms_2)
+print("pk = %u"% pk)
+print("rms_f = %u"% rms_f)
+
+plt.show()
