4edb4e59576fee879fe6c69708cb6cde229dfb69
Joe Burmeister <joe.burmeister@devtank.co.uk>
2021-12-23 20:09:36 +0000

    Missing checkin.

---
+++ flash_data.h
@@ -0,0 +1,28 @@
+#pragma once
+
+static void flash_set_data(const void * addr, const void * data, unsigned size)
+{
+    uintptr_t _addr = (uintptr_t)addr;
+
+    unsigned left_over = size % 8;
+    unsigned easy_size = size - left_over;
+
+    if (easy_size)
+        flash_program(_addr, (void*)data, easy_size);
+
+    if (size > easy_size)
+    {
+        const uint8_t * p = (const uint8_t*)data;
+
+        uint64_t v = 0;
+
+        for(unsigned n = 0; n < left_over; n+=1)
+        {
+             v |= (p[easy_size + n]) << (8*n);
+        }
+
+        flash_program_double_word(_addr + easy_size, v);
+    }
+}
+
+
