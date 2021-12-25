eae6f0dc3c389686f3f85ed4e211c187a277a33a
Joe Burmeister <joe.burmeister@devtank.co.uk>
2021-12-25 18:01:53 +0000

    First check of upgrade via Python binding manual test.

--- binding.py
+++ binding.py
@@ -37,125 +37,6 @@ class io_board_prop_t(object):
         return self._parent()
 
 
-class pps_t(io_board_prop_t):
-    @property
-    def values(self):
-        r = self.parent.command(b"pps %u" % self.index)
-        parts = r[0].split(b':')
-        pps = int(parts[1].strip())
-        parts = parts[2].split()
-        duty = int(parts[0]) / 10.0
-        return (pps, duty)
-
-
-class adc_t(io_board_prop_t):
-    def __init__(self, index, parent):
-        io_board_prop_t.__init__(self, index, parent)
-        self._min_value = 0
-        self._max_value = 0
-        self._avg_value = 0
-        self.unit       = None
-        self.name       = None
-        self.auto_refresh = True
-
-    def _raw_to_voltage(self, v):
-        debug_print("%sADC %u%s : %G%s" % (self.parent.log_prefix, self.index, "(%s)" % self.name if self.name else "", v, self.unit if self.unit else ""))
-        return v
-
-    def refresh(self):
-        if not self.auto_refresh:
-            return
-        r = self.parent.command(b"adc %u" % self.index)
-        self.update_from_line(r)
-
-    def update_from_line(self, r):
-        assert len(r) == 4
-        parts = [part.strip() for part in r[0].split(b':') ]
-        assert parts[0] == b"ADC"
-        assert int(parts[1].split(b' ')[0]) == self.index
-        min_str = r[1].split(b':')[1].strip()
-        dot = min_str.find(b".") + 1
-        if dot > 0:
-            while chr(min_str[dot]).isdecimal():
-                dot+=1
-            unit = min_str[dot:]
-            raw_min_value = float(min_str.replace(unit,b""))
-            raw_max_value = float(r[2].split(b':')[1].strip().replace(unit,b""))
-            raw_avg_value = float(r[3].split(b':')[1].strip().replace(unit,b""))
-            self._min_value = self._raw_to_voltage(raw_min_value)
-            self._max_value = self._raw_to_voltage(raw_max_value)
-            self._avg_value = self._raw_to_voltage(raw_avg_value)
-            self.unit = unit.decode()
-        else:
-            raw_min_value = int(min_str)
-            raw_max_value = int(r[2].split(b':')[1].strip())
-            avg_line = r[3].split(b':')[1].strip()
-            parts = avg_line.split(b"/")
-            assert len(parts) == 2
-            raw_avg_value = int(parts[0]) / float(parts[1])
-            self._min_value = self._raw_to_voltage(raw_min_value)
-            self._max_value = self._raw_to_voltage(raw_max_value)
-            self._avg_value = self._raw_to_voltage(raw_avg_value)
-            self.unit = None
-
-    @property
-    def value(self):
-        self.refresh()
-        return self._avg_value
-
-    @property
-    def values(self):
-        self.refresh()
-        return self._avg_value, self._min_value, self._max_value
-
-    @property
-    def calibration(self):
-        r = self.parent.command(b"cal %u" % self.index)
-        assert len(r) > 1
-        parts = [part.strip() for part in r[0].split(b':') ]
-        assert parts[0] == b"ADC"
-        assert int(parts[1].split(b' ')[0]) == self.index
-
-        if len(r) == 4:
-            scale = float(r[1].split(b':')[1].strip())
-            offset = float(r[2].split(b':')[1].strip())
-            unit = r[3].split(b':')[1].strip()
-            return [scale, offset, unit.decode()]
-        else:
-            assert r[1] == b"POLY"
-            count = len(r) - 3
-            end_line = r[-1]
-            assert end_line.startswith(b"Unit:")
-            unit = end_line[5:].strip()
-            polys = []
-            for n in range(0, count):
-                line = r[n + 2]
-                assert line.startswith(str(n).encode())
-                parts = line.split()
-                polys += [ float(parts[1].strip()) ]
-            return [polys, unit.decode()]
-
-    @calibration.setter
-    def calibration(self, v):
-        if len(v) == 3:
-            scale, offset, unit = v
-            r = self.parent.command(b"cal %uS:%0.06f" % (self.index, scale))
-            assert r[0] == b"Cal scale pending"
-            r = self.parent.command(b"cal %uO:%0.06f" % (self.index, offset))
-            assert r[0] == b"Cal offset pending"
-            r = self.parent.command(b"cal %uU:%s" % (self.index, _to_bytes(unit)))
-            assert r[0] == b"Cal set"
-        else:
-            assert len(v) == 2
-            polys = v[0]
-            unit = v[1]
-            for n in range(0, len(polys)):
-                r = self.parent.command(b"cal %u %u:%0.06f" % (self.index, n, polys[n]))
-                assert r[0] == b"Cal poly %u pending" % n
-            r = self.parent.command(b"cal %uU:%s" % (self.index, _to_bytes(unit)))
-            assert r[0] == b"Cal set"
-
-
 class io_t(io_board_prop_t):
     def __init__(self, index, parent):
         io_board_prop_t.__init__(self, index, parent)
@@ -315,130 +196,15 @@ class io_t(io_board_prop_t):
             assert value == v
 
 
-class uart_t(io_board_prop_t):
-    def __init__(self, index, parent):
-        io_board_prop_t.__init__(self, index, parent)
-        self._io = None
-        self._baud = 115200
-        self._parity = serial.PARITY_NONE
-        self._bytesize = serial.EIGHTBITS
-        self._stopbits = serial.STOPBITS_ONE
-        self._timeout = 1
-
-    def configure(self, baud, parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE, timeout=1):
-        self._baud = baud
-        self._parity = parity
-        self._bytesize = bytesize
-        self._stopbits = stopbits
-        self._timeout = timeout
-        if self._io:
-            self._io.close()
-            self._io = None
-
-    @property
-    def baud(self):
-        return self._baud
-
-    @property
-    def bytesize(self):
-        return self._bytesize
-
-    @property
-    def parity(self):
-        return self._parity
-
-    @property
-    def stopbits(self):
-        return self._stopbits
-
-    @property
-    def timeout(self):
-        return self._timeout
-
-    @property
-    def io(self):
-        if self._io:
-            return self._io
-
-        parent = self.parent
-
-        comm_port = parent.comm_port
-
-        num = "".join([ c if c in "0123456789" else "" for c in comm_port ])
-
-        base_name = comm_port[:-len(num)]
-
-        own_port = base_name + str(int(num) + self.index + 1)
-
-        debug_print("%sUART%u : %s" % (parent.log_prefix, self.index, own_port))
-
-        self._io = serial.Serial(port=own_port,
-                          baudrate=self._baud,
-                          parity=self._parity,
-                          stopbits=self._stopbits,
-                          bytesize=self._bytesize,
-                          timeout=self._timeout)
-
-        r = parent.command("uart %u" % self.index)
-        assert len(r) == 1, "Expected one line for uart response."
-        parts = r[0].split()
-        assert int(parts[1]) == self.index, "Wrong uart responed."
-        assert self._baud == int(parts[3]), "Wrong uart baud rate"
-        bits, parity, stopbits = parts[4]
-        assert self._bytesize == serial.SEVENBITS and bits == '7' or \
-               self._bytesize == serial.EIGHTBITS and bits == '8', \
-               "Bits not set as expected."
-        assert self._parity == serial.PARITY_NONE and parity == 'N' or \
-               self._parity == serial.PARITY_EVEN and parity == 'E' or \
-               self._parity == serial.PARITY_ODD and parity == 'O',\
-               "Parity not set as expected."
-        assert self._stopbits == serial.STOPBITS_ONE and stopbits == '1' or \
-               self._stopbits == serial.STOPBITS_TWO and stopbits == '2',\
-               "Parity not set as expected."
-
-        return self._io
-
-    def read(self, num):
-        r = self.io.read(num)
-        debug_print("%sUART%u >> %s" % (self.parent.log_prefix, self.index, r))
-        return r
-
-    def write(self, s):
-        self.io.write(s)
-        debug_print("%sUART%u << %s" % (self.parent.log_prefix, self.index, s))
-
-    def readline(self):
-        line = self.io.readline()
-        debug_print("%sUART%u >> %s" % (self.parent.log_prefix, self.index, line))
-        return line
-
-    def drain(self):
-        # Give enough time for it to be reasonable to expect a character
-        time.sleep(1 / (self._baud / 10) * 10)
-        while self.io.in_waiting:
-            self.io.read(self.io.in_waiting)
-            time.sleep(1 / (self._baud / 10) * 10)
-        debug_print("%sUART%u Drained" % (self.parent.log_prefix, self.index))
-
-
-
 
 class io_board_py_t(object):
     __LOG_START_SPACER = b"============{"
     __LOG_END_SPACER   = b"}============"
-    PROP_MAP = {b"ppss" : pps_t,
-                b"adcs" : adc_t,
-                b"ios"   : io_t,
-                b"uarts" : uart_t}
+    PROP_MAP = {b"ios"   : io_t}
     READTIME = 2
     NAME_MAP = {}
 
     def __init__(self, dev, dev_label = None):
-        self.ppss = []
-        self.inputs = []
-        self.outputs = []
-        self.uarts   = []
-        self.adcs = []
         self.ios = []
         self.NAME_MAP = type(self).NAME_MAP
         self.comm_port = dev
@@ -470,56 +236,6 @@ class io_board_py_t(object):
             io_obj = self.ios[io_index]
             io_obj.load_from_line(line)
 
-        for io_obj in self.ios:
-            if io_obj.label.startswith(b"PPS"):
-                index = int(io_obj.label[3:])
-                self.ppss[index].io_obj = io_obj
-            elif io_obj.label.startswith(b"UART"):
-                index = int(io_obj.label[4:-3])
-                if io_obj.label.endswith(b"RX"):
-                    self.uarts[index].rx_io_obj = io_obj
-                elif io_obj.label.endswith(b"TX"):
-                    self.uarts[index].tx_io_obj = io_obj
-
-    def use_ios_map(self, ios):
-        for n in range(0, len(ios)):
-            self.ios[n].setup(*ios[n])
-        r = self.command(b"count")
-        m = {}
-        for line in r:
-            parts = line.split(b':')
-            name  = parts[0].lower().strip()
-            if name in ["inputs", "outputs"]:
-                count = int(parts[1])
-                for n in range(0, count):
-                    child = type(self).PROP_MAP[name](n, self)
-                    children = getattr(self, name.decode())
-                    children += [child]
-
-    @property
-    def enable_calibdation(self):
-        r = self.command(b"en_cal")
-        assert len(r) == 1, "Invalid return count from en_cal command."
-        line = r[0]
-        assert line.startswith(b"ADC Cals "), "Invalid response from en_cal command."
-        if line.endswith(b"Enabled"):
-            return True
-        if line.endswith(b"Disabled"):
-            return False
-        assert False, "Invalid state in response from en_cal command."
-
-    @enable_calibdation.setter
-    def enable_calibdation(self, v):
-        v = int(bool(v))
-        r = self.command(b"en_cal %u" % v)
-        assert len(r) == 1, "Invalid return count set with en_cal command."
-        line = r[0]
-        if line == b"Enabled ADC Cals":
-            return True
-        if line == b"Disabled ADC Cals":
-            return False
-        assert False, "Invalid state in response set with en_cal command."
-
     def __getattr__(self, item):
         debug_print("%sBinding name lookup %s" %(self.log_prefix, item))
         getter = self.NAME_MAP.get(item, None)
@@ -527,54 +243,20 @@ class io_board_py_t(object):
             return getter(self)
         raise AttributeError("Attribute %s not found" % item)
 
-    def sync_all_adcs(self):
-        r = self.command(b"adcsw")
-        pos = 0
-        for adc in self.adcs:
-            adc.auto_refresh = False
-            adc.update_from_line(r[pos:pos+4])
-            pos += 4
-
-    @property
-    def adc_rate_ms(self):
-        r = self.command(b"adc_ms")
-        assert len(r) == 1, "Wrong ADC rate response."
-        parts = [ part.strip() for part in r[0].split(b':') ]
-        assert parts[0] == b"ADC sampling interval" and parts[1].endswith(b"ms"), "Wrong ADC rate response."
-        return int(parts[1][:-2])
-
-    @adc_rate_ms.setter
-    def adc_rate_ms(self, v):
-        r = self.command(b"adc_ms %u" % v)
-        assert len(r) == 1, "Wrong ADC rate response."
-        parts = [ part.strip() for part in r[0].split(b':') ]
-        assert parts[0] == b"ADC sampling interval" and parts[1].endswith(b"ms"), "Wrong ADC rate response."
-        check = int(parts[1][:-2])
-        assert check == v, "Unable to set ADC rate as requested."
-
-    @property
-    def cal_name(self):
-        r = self.command(b"cal_name")
-        assert len(r) == 1 and r[0][:2] == b"N:", "Invalid cal_name response."
-        return r[0][2:].decode()
-
-    @property
-    def adcs_calibration(self):
-        r = {"name" : self.cal_name, "adc_rate_ms" : self.adc_rate_ms}
-        for adc in self.adcs:
-            r[adc.index] = adc.calibration
-        return r
-
-    @adcs_calibration.setter
-    def adcs_calibration(self, cal):
-        for k, v in cal.items():
-            if k == "name":
-                r = self.command(b"persist %s" % _to_bytes(v))
-                assert (len(r) == 1 and r[0] == b"New persistent data started."), "Invalid persist response."
-            elif k == "adc_rate_ms":
-                self.adc_rate_ms = v
-            else:
-                self.adcs[k].calibration = v
+    def fw_upload(self, filmware):
+        from crccheck.crc import CrcModbus
+        data = open(filmware, "rb").read()
+        hdata = b"".join([b"%02x" % x for x in data])
+        crc = CrcModbus.calc(data)
+        for n in range(0, len(hdata), 24):
+            debug_print("Chunk %u/%u" % (n, len(hdata)))
+            r = self.command(b"fw+ "+hdata[n:n+24])
+            assert b"FW chunk added" in r
+        r = self.command(b"fw@ %04x" % crc)
+        assert b"FW added" in r
+        self.comm.write(b'reset\n')
+        self.comm.flush()
+        self.comm.drain()
 
     def _read_line(self):
         line = self.comm.readline().strip()
---
+++ upgrade.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+
+import sys
+from binding import io_board_py_t
+
+dev = io_board_py_t(sys.argv[1])
+dev.fw_upload(sys.argv[2])
