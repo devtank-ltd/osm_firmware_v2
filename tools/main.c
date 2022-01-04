558b0a85edded70c6067805aff9692c63161d433
Joe Burmeister <joe.burmeister@devtank.co.uk>
2022-01-04 16:02:31 +0000

    Update config blob tool so can make config of pending new firmware.

--- tools/main.c
+++ tools/main.c
@@ -6,11 +6,12 @@
 
 int main(int argc, char *argv[])
 {
-    persist_storage_t config = {PERSIST__VERSION, DEBUG_SYS, DEFAULT_FW_ADDR};
+    persist_storage_t config = {.version = PERSIST__VERSION,
+                                .log_debug_mask = DEBUG_SYS};
 
-    if (argc < 2)
+    if (argc < 3)
     {
-        fprintf(stderr, "Give filename to write.\n");
+        fprintf(stderr, "Give filename to write and any pending firmware size.\n");
     return EXIT_FAILURE;
     }
 
@@ -19,14 +20,16 @@ int main(int argc, char *argv[])
     if (!f)
     {
         perror("Failed to open file.");
-    return EXIT_FAILURE;
+        return EXIT_FAILURE;
     }
+
+    config.pending_fw = strtoul(argv[2], NULL, 10);
 
     if (fwrite(&config, sizeof(config), 1, f) != 1)
     {
         perror("Failed to write file.");
-    fclose(f);
-    return EXIT_FAILURE;
+        fclose(f);
+        return EXIT_FAILURE;
     }
 
     fclose(f);
