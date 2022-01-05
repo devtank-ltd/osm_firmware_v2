3f25ab6dd0f3518ff881294ce7177b94e38e9c24
Joe Burmeister <joe.burmeister@devtank.co.uk>
2022-01-05 16:13:05 +0000

    Tweaks to how HTU21D is done.

--- htu21d.c
+++ htu21d.c
@@ -22,15 +22,6 @@ typedef enum
 } htu21d_reg_t;
 
 
-typedef enum
-{
-    HTU21D_STATUS_OPENCIRCUIT   = 0,
-    HTU21D_STATUS_TEMP          = 0,
-    HTU21D_STATUS_HUMI          = 2,
-    HTU21D_STATUS_CLOSEDCIRCUIT = 3,
-} htu21d_status_t;
-
-
 uint8_t crc8(uint8_t* mem, uint8_t size)
 {
     uint8_t crc = 0x00;
@@ -50,34 +41,42 @@ uint8_t crc8(uint8_t* mem, uint8_t size)
 }
 
 
+#define htu21d_debug(...)  log_debug(DEBUG_TMP_HUM, "HTU21D: " __VA_ARGS__)
+
+
+
 static bool htu21d_read_reg16(htu21d_reg_t reg, uint16_t * r)
 {
     if (!r)
         return false;
     uint8_t reg8 = reg;
     uint8_t d[3] = {0};
+    htu21d_debug("Read command 0x%"PRIx8, reg8);
+
     i2c_transfer7(HTU21D_I2C, I2C_HTU21D_R_ADDR, &reg8, 1, d, 3);
-    htu21d_status_t stat = d[1] >> 6;
-    if (reg == HTU21D_TRIG_TEMP_MEAS || reg == HTU21D_HOLD_TRIG_TEMP_MEAS)
-    {
-        if (stat != HTU21D_STATUS_TEMP)
-            return false;
-    }
-    else if (reg == HTU21D_TRIG_HUMI_MEAS || reg == HTU21D_HOLD_TRIG_HUMI_MEAS)
-    {
-        if (stat != HTU21D_STATUS_HUMI)
-            return false;
-    }
-    *r = (d[0] << 6) | (d[1] & 0x3f);
+
+    htu21d_debug("Got 0x%"PRIx8" 0x%"PRIx8" 0x%"PRIx8, d[0], d[1], d[2]);
+
+    *r = ((d[0] & 0x3f) << 8) | d[1];
 
     // HTU21D(F) sensor provides a CRC-8 checksum for error detection. The polynomial used is X8 + X5 + X4 + 1.
     return (d[2] == crc8(d, 2));
 }
 
 
+static void htu21d_send(htu21d_reg_t reg)
+{
+    uint8_t reg8 = reg;
+    htu21d_debug("Send command 0x%"PRIx8, reg8);
+    i2c_transfer7(HTU21D_I2C, I2C_HTU21D_W_ADDR, &reg8, 1, NULL, 0);
+}
+
+
+
 void htu21d_init()
 {
     i2c_init(HTU21D_I2C_INDEX);
+    htu21d_send(HTU21D_SOFT_RESET);
 }
 
 
