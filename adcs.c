8dee4b7da5bbb4cb6d338e83d789b0f1441db626
Harry Geyer <harry.geyer@devtank.co.uk>
2022-01-07 17:09:26 +0000

    Reimplemented some functions to get current out.

--- adcs.c
+++ adcs.c
@@ -66,20 +66,16 @@ typedef struct
 } adc_dma_channel_t;
 
 
-typedef struct
-{
-    uint64_t sum;
-    uint32_t count;
-    uint16_t min;
-    uint16_t max;
-} adc_simple_val_t;
+typedef uint16_t all_adcs_buf_t[ADC_DMA_CHANNELS_COUNT][ADCS_NUM_SAMPLES*ADC_COUNT];
 
 
+static uint16_t                     adcs_buffer_pos                                     = 0;
+static all_adcs_buf_t               adcs_buffer;
 static bool                         adcs_running                                        = false;
-static adc_simple_val_t             adcs_val;
 static uint8_t                      adc_channel_array[ADC_COUNT]                        = ADC_CHANNELS;
 static uint16_t                     midpoint;
 static volatile adc_value_status_t  adc_value_status                                    = ADCS_VAL_STATUS_IDLE;
+static uint16_t                     peak_vals[ADCS_NUM_SAMPLES];
 
 
 uint32_t adcs_collection_time(void)
@@ -138,6 +134,169 @@ static void _adcs_setup_adc(void)
 bool adcs_set_midpoint(uint16_t new_midpoint)
 {
     midpoint = new_midpoint;
+    if (!persist_set_adc_midpoint(new_midpoint))
+    {
+        log_debug(DEBUG_ADC, "Could not set the persistent storage for the midpoint.");
+        return false;
+    }
+    return true;
+}
+
+
+#ifdef __ADC_RMS_FULL__
+static float _Q_rsqrt( float number )
+{
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5F;
+
+    x2 = number * 0.5F;
+    y  = number;
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wstrict-aliasing"
+    i  = * ( long * ) &y;                       // evil floating point bit level hacking
+    i  = 0x5f3759df - ( i >> 1 );               // what the fuck? 
+    y  = * ( float * ) &i;
+    #pragma GCC diagnostic pop
+    y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//  y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+    return y;
+}
+
+static bool _adcs_get_rms(all_adcs_buf_t buff, unsigned buff_len, uint16_t* adc_rms)
+{
+    uint64_t sum = 0;
+    for (unsigned i = 0; i < buff_len; i++)
+    {
+        sum += buff[0][i] * buff[0][i];
+    }
+    *adc_rms = midpoint - 1/_Q_rsqrt( ( sum / ADCS_NUM_SAMPLES ) - midpoint );
+    return true;
+}
+#else
+static bool _adcs_get_rms(all_adcs_buf_t buff, unsigned buff_len, uint16_t* adc_rms)
+{
+    /**
+    Four major steps:
+    * First find the peaks, this is done with the following criteria:
+        - adc val is smaller than previous
+        - new wave (and so move position/cursor) when before was below midpoint, now is above midpoint
+
+    * Calculate 'rough' average
+
+    * If peak values are larger than 30% of average then remove.
+      This will remove the small noises that may fit the criteria but not be true peaks.
+
+    * This value is divided by sqrt(2) to give the RMS.
+    */
+
+    uint32_t sum = 0;
+    uint16_t peak_pos = 0;
+    peak_vals[0] = buff[0][0];
+
+    // Find the peaks in the data
+    for (unsigned i = 1; i < buff_len; i++)
+    {
+        if (buff[0][i] < buff[0][i-1])
+        {
+            peak_vals[peak_pos] = buff[0][i];
+        }
+        else if (buff[0][i-1] <= midpoint && buff[0][i] > midpoint)
+        {
+            sum += peak_vals[peak_pos];
+            peak_vals[++peak_pos] = buff[0][i];
+        }
+    }
+
+    // Early exit if nothing found
+    if (peak_pos == 0)
+    {
+        log_debug(DEBUG_ADC, "Cannot find any peaks.");
+        return false;
+    }
+    uint32_t inter_val = midpoint - sum / peak_pos;
+    inter_val /= sqrt(2);
+    *adc_rms = midpoint - inter_val;
+    return true;
+}
+#endif //__ADC_RMS_FULL__
+
+
+static bool _adcs_to_mV(uint16_t value, uint16_t* mV)
+{
+    // Linear scale without calibration.
+    // ADC of    0 -> 0V
+    //        4096 -> 3.3V
+
+    uint16_t max_value = 4095;
+    uint16_t min_value = 0;
+    uint16_t max_mV = 3300;
+    uint16_t min_mV = 0;
+    uint32_t inter_val;
+
+    inter_val = value * (max_mV - min_mV);
+    inter_val /= (max_value - min_value);
+
+    if (inter_val > UINT16_MAX)
+    {
+        log_debug(DEBUG_ADC, "Cannot downsize value '%"PRIu32"'.", inter_val);
+        return false;
+    }
+
+    *mV = (uint16_t)inter_val;
+    return true;
+}
+
+
+static bool _adcs_current_clamp_conv(uint16_t adc_val, uint16_t* cc_mA)
+{
+    /**
+     First must calculate the peak voltage
+     V_pk = Midpoint - V_adc
+
+     Next convert peak voltage to RMS. For DC this is "=" to it. Kind of.
+     For AC it can approximated by V_RMS = 1/sqrt(2) * V_peak
+
+     Now converting from a voltage to a current we use our trusty
+     V = I * R      - >     I = V / R
+     The resistor used is 22 Ohms.
+
+     The current is scaled by the current clamp to the effect of
+     I_t = 2000 * I.
+
+     To retain precision, multiplications should be done first (after casting to
+     a uint32_t and then divisions after.
+    */
+    uint32_t inter_value    = 0;
+    uint16_t adc_diff       = 0;
+
+    // If adc_val is larger, then pretend it is at the midpoint
+    if (adc_val < midpoint)
+    {
+        adc_diff = midpoint - adc_val;
+    }
+
+    // Once the conversion is no longer linearly multiplicative this needs to be changed.
+    if (!_adcs_to_mV(adc_diff, (uint16_t*)&inter_value))
+    {
+        log_debug(DEBUG_ADC, "Cannot get mV value of midpoint.");
+        return false;
+    }
+
+    if (inter_value > UINT32_MAX / 2000)
+    {
+        log_debug(DEBUG_ADC, "Overflowing value.");
+        return false;
+    }
+    inter_value *= 2000;
+    inter_value /= 22;
+    if (inter_value > UINT16_MAX)
+    {
+        log_debug(DEBUG_ADC, "Cannot downsize value '%"PRIu32"'.", inter_value);
+        return false;
+    }
+    *cc_mA = (uint16_t)inter_value;
     return true;
 }
 
@@ -151,28 +310,20 @@ void adcs_loop_iteration(void)
         adc_start_conversion_regular(ADC1);
         while (!(adc_eoc(ADC1)));
         uint16_t new_reading = adc_read_regular(ADC1);
-        if (adcs_val.count == 0)
+
+        adcs_buffer[0][adcs_buffer_pos++] = new_reading;
+        if (adcs_buffer_pos == sizeof(adcs_buffer[0]) / sizeof(uint16_t))
         {
-            adcs_val.min = new_reading;
-            adcs_val.max = new_reading;
+            adcs_running = false;
         }
-        else if (adcs_val.min > new_reading)
-        {
-            adcs_val.min = new_reading;
-        }
-        else if (adcs_val.max < new_reading)
-        {
-            adcs_val.max = new_reading;
-        }
-        adcs_val.sum += new_reading;
-        adcs_val.count++;
     }
 }
 
 
 bool adcs_begin(char* name)
 {
-    memset(&adcs_val, 0, sizeof(adc_simple_val_t));
+    adcs_buffer_pos = 0;
+    memset(&adcs_buffer, 0, sizeof(all_adcs_buf_t));
     adcs_running = true;
     return true;
 }
@@ -187,12 +338,12 @@ bool adcs_calibrate_current_clamp(void)
 bool adcs_get_cc(char* name, value_t* value)
 {
     adcs_running = false;
-    if (adcs_val.count == 0)
-    {
-        return false;
-    }
-    uint64_t avg = adcs_val.sum / adcs_val.count;
-    *value = value_from_u64(avg);
+
+    uint16_t adcs_rms = 0;
+    uint16_t cc_mA = 0;
+    _adcs_get_rms(adcs_buffer, adcs_buffer_pos, &adcs_rms);
+    _adcs_current_clamp_conv(adcs_rms, &cc_mA);
+    *value = value_from_u16(cc_mA);
     return true;
 }
 
