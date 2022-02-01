6a4cc670e7103dcfa7be7605796fa651a3b0d992
Joe Burmeister <joe.burmeister@devtank.co.uk>
2022-02-01 15:13:07 +0000

    Init light driver.

--- veml7700.c
+++ veml7700.c
@@ -85,6 +85,34 @@ typedef enum
     VEML7700_CMD_ALS_INT        = 0x06,
 } veml7700_cmd_t;
 
+typedef union
+{
+    struct
+    {
+        uint16_t ald_sd:1;
+        uint16_t als_int_en:1;
+        uint16_t _:2;
+        uint16_t als_pers:2;
+        uint16_t als_it:4;
+        uint16_t __:1;
+        uint16_t als_sm:2;
+        uint16_t ___:3;
+    };
+    uint16_t raw;
+} __attribute__((__packed__)) veml7700_cmd_0_t;
+
+typedef union
+{
+    struct
+    {
+        uint16_t psm_en:1;
+        uint16_t psm:2;
+        uint16_t _:13;
+    };
+    uint16_t raw;
+} __attribute__((__packed__)) veml7700_cmd_3_t;
+
+
 
 static uint16_t     veml7700_conf_reg_val       = VEML7700_ALS_GAIN_1 | VEML7700_ALS_IT_100 | VEML7700_ALS_PERS_1 | VEML7700_ALS_INT_EN_DISABLED;
 static uint16_t     veml7700_conf_power         = VEML7700_PSM_MODE_4 | VEML7700_PSM_EN_ENABLE;
@@ -106,21 +134,25 @@ static bool _veml7700_read_reg16(veml7700_cmd_t reg, uint16_t * r)
     uint8_t reg8 = reg;
     uint8_t d[2] = {0};
     light_debug("Read command 0x%"PRIx8, reg8);
-
-    i2c_transfer7(VEML7700_I2C, I2C_VEML7700_ADDR, &reg8, 1, d, 2);
+    if (!i2c_transfer_timeout(VEML7700_I2C, I2C_VEML7700_ADDR, &reg8, 1, d, 2, 1000))
+    {
+        veml7700_init();
+        return false;
+    }
     _veml7700_get_u16(d, r);
     return true;
 }
 
 
-static void _veml7700_write_reg16(veml7700_cmd_t reg, uint16_t data)
+static bool _veml7700_write_reg16(veml7700_cmd_t reg, uint16_t data)
 {
-    uint8_t reg8 = reg;
-    uint8_t data16[2];
-    data16[0] = data && 0x00FF;
-    data16[1] = data && 0xFF00;
-    light_debug("Send command 0x%"PRIx8" 0x%"PRIx16, reg8, data);
-    i2c_transfer7(VEML7700_I2C, I2C_VEML7700_ADDR, &reg8, 1, data16, 2);
+    uint8_t payload[3] = { reg, (data && 0xFF), (data >> 8) };
+    light_debug("Send command 0x%"PRIx8" 0x%"PRIx16, (uint8_t)reg, data);
+    if (!i2c_transfer_timeout(VEML7700_I2C, I2C_VEML7700_ADDR, payload, 3, NULL, 0, 1000))
+    {
+        return false;
+    }
+    return true;
 }
 
 
@@ -138,8 +170,7 @@ uint32_t veml7700_measurements_collection_time(void)
 
 bool veml7700_light_measurements_init(char* name)
 {
-    _veml7700_write_reg16(VEML7700_CMD_ALS_CONF_0, veml7700_conf_reg_val | VEML7700_ALS_SD_ON);
-    return true;
+    return _veml7700_write_reg16(VEML7700_CMD_ALS_CONF_0, veml7700_conf_reg_val | VEML7700_ALS_SD_ON);
 }
 
 
@@ -158,8 +189,15 @@ bool veml7700_light_measurements_get(char* name, value_t* value)
 void veml7700_init(void)
 {
     i2c_init(VEML7700_I2C_INDEX);
-    _veml7700_write_reg16(VEML7700_CMD_ALS_CONF_0, veml7700_conf_reg_val | VEML7700_ALS_SD_OFF);
-    _veml7700_write_reg16(VEML7700_CMD_POWER_SAVING, veml7700_conf_power);
+
+    veml7700_cmd_0_t cmd0 = {.ald_sd = 0, .als_int_en = 0, .als_pers = 0, .als_it = 0, .als_sm = 1};
+    veml7700_cmd_3_t cmd3 = {.psm_en = 0, .psm = 2};
+
+    if (_veml7700_write_reg16(VEML7700_CMD_ALS_CONF_0, cmd0.raw) &&
+        _veml7700_write_reg16(VEML7700_CMD_ALS_WH, 0x0000) &&
+        _veml7700_write_reg16(VEML7700_CMD_ALS_WL, 0XFFFF) &&
+        _veml7700_write_reg16(VEML7700_CMD_POWER_SAVING, cmd3.raw))
+        light_debug("Init'ed");
 }
 
 
