e190e0f0c5e30e7a1e0243eeb4a85f32d6929996
Harry Geyer <harry.geyer@devtank.co.uk>
2022-02-22 16:50:35 +0000

    Changed the pin mappings for CC2 and CC3 to fit the hardware.

--- pinmap.h
+++ pinmap.h
@@ -26,16 +26,16 @@
 #define ADCS_PORT_N_PINS                            \
 {                                                   \
     {GPIOA, GPIO1},      /* ADC 1  = Channel 6  */  \
-    {GPIOA, GPIO4},      /* ADC 1  = Channel 9  */  \
     {GPIOB, GPIO1},      /* ADC 1  = Channel 16 */  \
+    {GPIOA, GPIO4},      /* ADC 1  = Channel 9  */  \
     {GPIOC, GPIO0},      /* ADC 1  = Channel 1  */  \
     {GPIOC, GPIO2},      /* ADC 1  = Channel 3  */  \
     {GPIOC, GPIO3},      /* ADC 1  = Channel 4  */  \
 }
 
 #define ADC1_CHANNEL_CURRENT_CLAMP_1    6
-#define ADC1_CHANNEL_CURRENT_CLAMP_2    9
-#define ADC1_CHANNEL_CURRENT_CLAMP_3    16
+#define ADC1_CHANNEL_CURRENT_CLAMP_2    16
+#define ADC1_CHANNEL_CURRENT_CLAMP_3    9
 #define ADC1_CHANNEL_BAT_MON            1
 #define ADC1_CHANNEL_3V3_RAIL_MON       3
 #define ADC1_CHANNEL_5V_RAIL_MON        4
