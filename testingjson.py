34419605c455e7c93340098c8610a4bd4b61c648
Marcus Holder <marcus.holder@devtank.co.uk>
2022-02-28 16:45:02 +0000

    Function save_config tested and working.

--- testingjson.py
+++ testingjson.py
@@ -4,8 +4,9 @@ import string
 import os
 
 def load_config_json():
-    with open("tmp/my_osm_config.json", "r+") as jfile:
+    with open("/tmp/my_osm_config.json", "r+") as jfile:
         data = json.load(jfile)
+
 
 def app_key_generator(size=32, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -15,20 +16,22 @@ def dev_eui_generator(size=16, chars=string.ascii_lowercase + string.digits):
 
 def save_config():
     #Save an OSM's config as JSON
-    os.system("./scripts/config_save.sh /tmp/my_osm_config.img")
+    os.system("sudo ./scripts/config_save.sh /tmp/my_osm_config.img")
     os.system("./tools/build/json_x_img /tmp/my_osm_config.img > /tmp/my_osm_config.json")
 
     #Generate a random dev eui and app key
     app_key = app_key_generator()
     dev_eui = dev_eui_generator()
 
-    data = load_config_json()
+    with open("/tmp/my_osm_config.json", "r+") as jfile:
+        data = json.load(jfile)
+    print(data)
     data["lw_dev_eui"] = dev_eui
     data["lw_app_key"] = app_key
-    for item in data['measurements'][item]['interval']:
+    for item in data['measurements']:
         if data['measurements'][item]['interval'] == 1:
             data['measurements'][item]['interval'] = 0
-    with open("tmp/my_osm_config.json", 'w') as nfile:
+    with open("/tmp/my_osm_config.json", 'w') as nfile:
         json.dump(data, nfile, indent=2)
 
 
@@ -39,7 +42,7 @@ def set_pulse_count_special():
             data['ios'][item]['pull'] = 'NONE'
             data['ios'][item]['direction'] = 'IN'
             data['ios'][item]['use_special'] = True
-            with open("tmp/my_osm_config.json", 'w') as nfile:
+            with open("/tmp/my_osm_config.json", 'w') as nfile:
                 json.dump(data, nfile, indent=2)
 
 def set_onewire_special():
@@ -49,7 +52,7 @@ def set_onewire_special():
             data['ios'][item]['pull'] = 'NONE'
             data['ios'][item]['direction'] = 'IN'
             data['ios'][item]['use_special'] = True
-            with open("tmp/my_osm_config.json", 'w') as nfile:
+            with open("/tmp/my_osm_config.json", 'w') as nfile:
                 json.dump(data, nfile, indent=2)
 
 def restore_config():
@@ -57,7 +60,7 @@ def restore_config():
     for item in data['measurements'][item]['interval']:
         if data['measurements'][item]['interval'] == 0:
             data['measurements'][item]['interval'] = 1
-            with open("tmp/my_osm_config.json", 'w') as nfile:
+            with open("/tmp/my_osm_config.json", 'w') as nfile:
                 json.dump(data, nfile, indent=2)
     #Write out the JSON to the OSM
     os.system("./tools/build/json_x_img /tmp/my_osm_config.img < /tmp/my_osm_config.json")
