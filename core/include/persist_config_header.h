c9ab989889337572f2e012e9b44e0347fa1ab061
Joe Burmeister <joe.burmeister@devtank.co.uk>
2022-03-10 11:01:37 +0000

    Fix silly alignment bug after review changes! Doh!

--- core/include/persist_config_header.h
+++ core/include/persist_config_header.h
@@ -38,9 +38,9 @@ typedef struct
     char                    lw_app_key[LW_APP_KEY_LEN];
     measurements_def_t      measurements_arr[MEASUREMENTS_MAX_NUMBER];
     uint16_t                cc_midpoints[ADC_CC_COUNT];
-    uint16_t                __;
-    uint64_t                ___;
+    uint16_t                _[8-(ADC_CC_COUNT%8)];
     uint16_t                ios_state[IOS_COUNT];
+    uint16_t                __[8-(IOS_COUNT%8)];
     float                   sai_cal_coeffs[SAI_NUM_CAL_COEFFS];
 } __attribute__((__packed__)) persist_storage_t;
 
