7cee2936231703c78ef26fb63bb2feb1a8d3586e
Harry Geyer <harry.geyer@devtank.co.uk>
2022-06-28 17:01:57 +0100

    Changed the watchdog time to be 10s because of long light reading etc.

--- core/include/config.h
+++ core/include/config.h
@@ -130,7 +130,7 @@ _Static_assert(0, "Not given a STM model.");
 
 #define __ADC_RMS_FULL__
 
-#define IWDG_NORMAL_TIME_MS 5000
+#define IWDG_NORMAL_TIME_MS 10000
 #define IWDG_MAX_TIME_MS    32760
 #define SLEEP_MAX_TIME_MS   30000
 
