7309efdbb1a628ff89b80a021898641a27c7f93a
Harry Geyer <harry.geyer@devtank.co.uk>
2022-09-09 14:20:18 +0100

    Emptied out the pulsecounting for the linux build.

--- linux/sensors/src/pulsecount.c
+++ linux/sensors/src/pulsecount.c
@@ -1,1 +1,51 @@
-../../../sensors/src/pulsecount.c
+#include <inttypes.h>
+
+#include "pulsecount.h"
+
+
+#define PULSECOUNT_COLLECTION_TIME_MS       1000;
+
+
+void pulsecount_init(void)
+{
+}
+
+
+void pulsecount_enable(bool enable)
+{
+    if (enable)
+        pulsecount_init();
+}
+
+
+void pulsecount_log()
+{
+}
+
+
+measurements_sensor_state_t pulsecount_collection_time(char* name, uint32_t* collection_time)
+{
+    if (!collection_time)
+    {
+        return MEASUREMENTS_SENSOR_STATE_ERROR;
+    }
+    *collection_time = PULSECOUNT_COLLECTION_TIME_MS;
+    return MEASUREMENTS_SENSOR_STATE_SUCCESS;
+}
+
+
+measurements_sensor_state_t pulsecount_begin(char* name)
+{
+    return MEASUREMENTS_SENSOR_STATE_SUCCESS;
+}
+
+
+measurements_sensor_state_t pulsecount_get(char* name, measurements_reading_t* value)
+{
+    return MEASUREMENTS_SENSOR_STATE_ERROR;
+}
+
+
+void pulsecount_ack(char* name)
+{
+}
