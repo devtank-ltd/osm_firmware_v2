68bb559d858483819c22aea013da0e5a65f21392
Marcus Holder <marcus.holder@devtank.co.uk>
2022-10-13 09:44:54 +0100

    Added revision table into config gui manual, updated files to tar in create_gui_exe.sh.

--- config_gui/release/create_gui_exe.sh
+++ config_gui/release/create_gui_exe.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-pyinstaller --onefile config_gui.py --hidden-import='PIL._tkinter_finder' 
+pyinstaller --onefile config_gui.py --hidden-import='PIL._tkinter_finder'
 
 mv dist/config_gui .
 
 staticx config_gui OSM_Configuration_Gui
 
- tar -czvf osm_gui_bundle.tar.gz OSM_Configuration_Gui config_database/ complete.bin stm32flash yaml_files/ osm_pictures/ ../config_gui_manual.pdf ../README.md ../create_static_stm.sh static_program.sh
+ tar -czvf osm_gui_bundle.tar.gz OSM_Configuration_Gui config_database/ complete.bin stm32flash yaml_files/ osm_pictures/ config_gui_manual.pdf ../README.md ../create_static_stm.sh static_program.sh
 
 cp osm_gui_bundle.tar.gz ~/config_gui_bundle/
 
