ce0062d4eb1db558aa469282dc8b724d3d81fab2
Harry Geyer <harry.geyer@devtank.co.uk>
2022-10-25 10:13:42 +0100

    Deiniting the w1 client.

--- linux/core/src/linux.c
+++ linux/core/src/linux.c
@@ -453,6 +453,7 @@ static void _linux_exit(int err)
     pthread_join(_linux_listener_thread_id, NULL);
     fprintf(stdout, "Cleaning up before exit...\n");
     i2c_linux_deinit();
+    w1_linux_deinit();
     _linux_cleanup_fd_handlers();
     fprintf(stdout, "Finished.\n");
 }
