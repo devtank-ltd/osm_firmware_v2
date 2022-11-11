970d8e2582c4a3a4bbb0ad72556e84c99d231fec
Joe Burmeister <joe.burmeister@devtank.co.uk>
2022-11-11 11:27:01 +0000

    Catch when sending modbus messages too fast.

--- core/src/modbus.c
+++ core/src/modbus.c
@@ -345,6 +345,16 @@ bool modbus_start_read(modbus_reg_t * reg)
                 return false;
             }
         }
+        else
+        {
+            uint32_t delta = since_boot_delta(get_since_boot_ms(), modbus_cur_send_time);
+            if (delta < MODBUS_RESP_TIMEOUT_MS)
+            {
+                modbus_debug("No slot free.. sending to fast??");
+                return false;
+            }
+        }
+
         modbus_debug("Previous comms issue. Restarting slots.");
         ring_buf_clear(&_message_queue);
 
