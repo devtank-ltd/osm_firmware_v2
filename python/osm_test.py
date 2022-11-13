79a84111e95b6d41028afc01988446ea9bea95b2
Joe Burmeister <joe.burmeister@excelsiorelectronics.com>
2022-11-13 20:05:11 +0000

    Add PM10 & PM25 back into measurements in automated test.

--- python/osm_test.py
+++ python/osm_test.py
@@ -187,6 +187,8 @@ class test_framework_t(object):
         if not self._connect_osm(self.DEFAULT_DEBUG_PTY_PATH):
             return False
         self._vosm_conn.measurements_enable(False)
+        self._vosm_conn.PM10.interval = 1
+        self._vosm_conn.PM25.interval = 1
 
         self._vosm_conn.setup_modbus(is_bin=True)
         self._vosm_conn.setup_modbus_dev(5, "E53", True, True, [
