f3ab1b5d1a0eaa95bb8340f8ec6d1f22c9b9fb2c
Harry Geyer <harry.geyer@devtank.co.uk>
2022-11-23 14:59:42 +0000

    Updated sens01 pinmap

--- model/sens01/sens01_pinmap.h
+++ model/sens01/sens01_pinmap.h
@@ -11,13 +11,32 @@
     {GPIOC, GPIO3},      /* ADC 1  = Channel 4  */  \
 }
 
-#define SENS01_ADC1_CHANNEL_FTMA_1             6
-#define SENS01_ADC1_CHANNEL_FTMA_2            16
-#define SENS01_ADC1_CHANNEL_FTMA_3             9
-#define SENS01_ADC1_CHANNEL_FTMA_4            10
+#define ADC1_CHANNEL_FTMA_1                    6
+#define ADC1_CHANNEL_FTMA_2                    16
+#define ADC1_CHANNEL_FTMA_3                    9
+#define ADC1_CHANNEL_FTMA_4                    10
 #define SENS01_ADC1_CHANNEL_BAT_MON            1
 #define SENS01_ADC1_CHANNEL_3V3_RAIL_MON       3
 #define SENS01_ADC1_CHANNEL_5V_RAIL_MON        4
+
+#define SENS01_ADC_INDEX_FTMA_1            0
+#define SENS01_ADC_INDEX_FTMA_2            1
+#define SENS01_ADC_INDEX_FTMA_3            2
+#define SENS01_ADC_INDEX_FTMA_4            3
+#define SENS01_ADC_INDEX_BAT_MON           4
+#define SENS01_ADC_INDEX_3V3_RAIL_MON      5
+#define SENS01_ADC_INDEX_5V_RAIL_MON       6
+
+#define SENS01_ADC_CHANNELS  { SENS01_ADC1_CHANNEL_FTMA_1,          \
+                               SENS01_ADC1_CHANNEL_FTMA_2,          \
+                               SENS01_ADC1_CHANNEL_FTMA_3,          \
+                               SENS01_ADC1_CHANNEL_FTMA_4,          \
+                               SENS01_ADC1_CHANNEL_BAT_MON,         \
+                               SENS01_ADC1_CHANNEL_3V3_RAIL_MON,    \
+                               SENS01_ADC1_CHANNEL_5V_RAIL_MON      }
+#define ADC_FTMA_CHANNELS { SENS01_ADC1_CHANNEL_CURRENT_CLAMP_1,  \
+                            SENS01_ADC1_CHANNEL_CURRENT_CLAMP_2,  \
+                            SENS01_ADC1_CHANNEL_CURRENT_CLAMP_3   }
 
 #define SENS01_ADC_DMA_CHANNELS                                                        \
 {                                                                               \
@@ -28,3 +47,9 @@
 
 #define SENS01_ADC_COUNT       7
 #define ADC_FTMA_COUNT  4
+
+
+#define ADC_TYPES_ALL_FTMA { ADCS_TYPE_FTMA1,    \
+                             ADCS_TYPE_FTMA2,    \
+                             ADCS_TYPE_FTMA3,    \
+                             ADCS_TYPE_FTMA4     }
