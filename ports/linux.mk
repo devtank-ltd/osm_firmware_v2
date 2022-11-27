add62b2073d818a04d3854d0bec13a379984039c
Joe Burmeister <joe.burmeister@devtank.co.uk>
2022-11-27 17:15:12 +0000

    Copy Linux peripheral scripts to build.

--- ports/linux.mk
+++ ports/linux.mk
@@ -24,7 +24,7 @@ LINUX_LDFLAGS = -Wl,--start-group -lc -lgcc -lm -Wl,--end-group -Wl,--gc-section
 LINUX_LDFLAGS += -lgcov --coverage -pthread -lutil
 
 
-SQLITE_DB = $(OSM_DIR)/config_gui/release/config_database/modbus_templates
+SQLITE_DB ?= $(OSM_DIR)/config_gui/release/config_database/modbus_templates
 
 define LINUX_FIRMWARE
 $(1)_UP_NAME=$$(shell echo $(1) | tr a-z A-Z)
@@ -38,6 +38,18 @@ $(1)_NOSM_OBJS=$$($(1)_NOSM_SRC:$$(MODEL_DIR)/%.c=$$(BUILD_DIR)/$(1)/%.o)
 
 $(1)_OBJS:= $$($(1)_IOSM_OBJS) $$($(1)_NOSM_OBJS)
 
+$(1)_PERIPHERALS_SRC:=$$(shell find $$(OSM_DIR)/ports/linux/peripherals -name "*.py")
+
+$$(info src $$($(1)_PERIPHERALS_SRC))
+
+$(1)_PERIPHERALS_DST:=$$($(1)_PERIPHERALS_SRC:$$(OSM_DIR)/ports/linux/%=$$(BUILD_DIR)/$(1)/%)
+
+$$(info dst $$($(1)_PERIPHERALS_DST))
+
+$$($(1)_PERIPHERALS_DST): $$(BUILD_DIR)/$(1)/% : $$(OSM_DIR)/ports/linux/%
+	mkdir -p "$$(@D)"
+	cp $$< $$@
+
 $$($(1)_IOSM_OBJS): $$(BUILD_DIR)/$(1)/%.o: $$(OSM_DIR)/%.c $$(BUILD_DIR)/.git.$$(GIT_COMMIT)
 	mkdir -p "$$(@D)"
 	$$(LINUX_CC) -c -Dfw_name=$(1) -DFW_NAME=$$($(1)_UP_NAME) $$(LINUX_CFLAGS) -I$$(MODEL_DIR)/$(1) $$(LINUX_INCLUDE_PATHS) $$< -o $$@
@@ -47,13 +59,14 @@ $$($(1)_NOSM_OBJS): $$(BUILD_DIR)/$(1)/%.o: $$(MODEL_DIR)/%.c $$(BUILD_DIR)/.git
 	$$(LINUX_CC) -c -Dfw_name=$(1) -DFW_NAME=$$($(1)_UP_NAME) $$(LINUX_CFLAGS) -I$$(MODEL_DIR)/$(1) $$(LINUX_INCLUDE_PATHS) $$< -o $$@
 
 
-$$(BUILD_DIR)/$(1)/firmware.elf: $$($(1)_OBJS)
+$$(BUILD_DIR)/$(1)/firmware.elf: $$($(1)_OBJS) $$($(1)_PERIPHERALS_DST)
 	$$(LINUX_CC) $$($(1)_OBJS) $$(LINUX_LDFLAGS) -o $$@
 
 $$(BUILD_DIR)/$(1)/.complete: $$(BUILD_DIR)/$(1)/firmware.elf
 	touch $$@
 
 $(1): $$(BUILD_DIR)/$(1)/.complete
+
 
 $(1)_test: $$(BUILD_DIR)/$(1)/firmware.elf
 	mkdir -p /tmp/osm/
