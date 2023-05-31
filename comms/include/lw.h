ef0e5b90a9544d4ebc74f09686bdc0bf7c180a5b
Harry Geyer <harry.geyer@devtank.co.uk>
2023-05-31 11:41:15 +0100

    Added a region selection for LoRaWAN in the command interface.

--- comms/include/lw.h
+++ comms/include/lw.h
@@ -15,12 +15,46 @@
 
 #define LW_DEV_EUI_LEN                      16
 #define LW_APP_KEY_LEN                      32
+#define LW_REGION_LEN                       7
+
+
+#define LW_REGION_NAME_EU433                "EU433"
+#define LW_REGION_NAME_CN470                "CN470"
+#define LW_REGION_NAME_RU864                "RU864"
+#define LW_REGION_NAME_IN865                "IN865"
+#define LW_REGION_NAME_EU868                "EU868"
+#define LW_REGION_NAME_US915                "US915"
+#define LW_REGION_NAME_AU915                "AU915"
+#define LW_REGION_NAME_KR920                "KR920"
+#define LW_REGION_NAME_AS923_1              "AS923-1"
+#define LW_REGION_NAME_AS923_2              "AS923-2"
+#define LW_REGION_NAME_AS923_3              "AS923-3"
+#define LW_REGION_NAME_AS923_4              "AS923-4"
+
+
+typedef enum
+{
+    LW_REGION_EU433    = 0 ,
+    LW_REGION_CN470    = 1 ,
+    LW_REGION_RU864    = 2 ,
+    LW_REGION_IN865    = 3 ,
+    LW_REGION_EU868    = 4 ,
+    LW_REGION_US915    = 5 ,
+    LW_REGION_AU915    = 6 ,
+    LW_REGION_KR920    = 7 ,
+    LW_REGION_AS923_1  = 8 ,
+    LW_REGION_AS923_2  = 9 ,
+    LW_REGION_AS923_3  = 10,
+    LW_REGION_AS923_4  = 11,
+    LW_REGION_MAX      = LW_REGION_AS923_4,
+} lw_region_t;
 
 
 typedef struct
 {
     char    dev_eui[LW_DEV_EUI_LEN];
     char    app_key[LW_APP_KEY_LEN];
+    uint8_t region; /* lw_region_t */
 } lw_config_t;
 
 
