5eb7c0a7fcee3c4ef2be094de1dd0f7e255d933c
joe.a.burmeister@gmail.com <joe.a.burmeister@gmail.com>
2023-06-15 21:57:39 +0100

    Get ESP persist working.

--- ports/esp32/src/persist_config.c
+++ ports/esp32/src/persist_config.c
@@ -11,6 +11,10 @@
 #include "platform.h"
 #include "platform_model.h"
 
+#define OSM_NS   "osm_ns"
+#define OSM_DATA "osm_data"
+#define OSM_MEAS "osm_meas"
+
 
 persist_storage_t               persist_data __attribute__((aligned (16))) = {0};
 persist_measurements_storage_t  persist_measurements __attribute__((aligned (16))) = {0};
@@ -36,15 +40,20 @@ void     persist_commit()
     esp_err_t err;
     nvs_handle_t config_handle;
 
-    ESP_ERROR_CHECK(nvs_open("osmconfig", NVS_READWRITE, &config_handle));
+    err = nvs_open(OSM_NS, NVS_READWRITE, &config_handle);
+    if (err != ESP_OK)
+    {
+        log_error("Failed to open %s to write : %s", OSM_NS, esp_err_to_name(err));
+        return;
+    }
 
     nvs_erase_all(config_handle);
 
-    err = nvs_set_blob(config_handle, "osm_data", &persist_data, sizeof(persist_data));
+    err = nvs_set_blob(config_handle, OSM_DATA, &persist_data, sizeof(persist_data));
     if (err != ESP_OK)
         log_error("Failed write persist_data : %s", esp_err_to_name(err));
 
-    err = nvs_set_blob(config_handle, "osm_meas", &persist_measurements, sizeof(persist_measurements));
+    err = nvs_set_blob(config_handle, OSM_MEAS, &persist_measurements, sizeof(persist_measurements));
     if (err != ESP_OK)
         log_error("Failed write persist_measurements : %s", esp_err_to_name(err));
     nvs_commit(config_handle);
@@ -64,10 +73,19 @@ bool persistent_init(void)
 
     nvs_handle_t config_handle;
 
-    ESP_ERROR_CHECK(nvs_open("osmconfig", NVS_READONLY, &config_handle));
+    err = nvs_open(OSM_NS, NVS_READONLY, &config_handle);
+    if (err != ESP_OK)
+    {
+        log_error("Failed to open %s to read : %s", OSM_NS, esp_err_to_name(err));
+        goto setup_exit;
+    }
+
     size_t persist_data_read = 0, persist_measurements_read = 0;
-    nvs_get_blob(config_handle, "osm_data", &persist_data, &persist_data_read);
-    nvs_get_blob(config_handle, "osm_meas", &persist_measurements, &persist_measurements_read);
+    /* Shouldn't need two calls, but does. */
+    nvs_get_blob(config_handle, OSM_DATA, NULL, &persist_data_read);
+    nvs_get_blob(config_handle, OSM_DATA, &persist_data, &persist_data_read);
+    nvs_get_blob(config_handle, OSM_MEAS, NULL, &persist_measurements_read);
+    nvs_get_blob(config_handle, OSM_MEAS, &persist_measurements, &persist_measurements_read);
 
     nvs_close(config_handle);
 
@@ -77,8 +95,6 @@ bool persistent_init(void)
         goto setup_exit;
     }
 
-    log_sys_debug("Loaded osm_data :%d", (int)persist_data_read);
-    log_sys_debug("Loaded osm_meas :%d", (int)persist_measurements_read);
     if (persist_data.version != PERSIST_VERSION)
     {
         log_error("Invalid config loaded (version %u)", persist_data.version);
