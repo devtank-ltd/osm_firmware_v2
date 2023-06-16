7f58b4164aea4550afd69bac8b5ad04f3aa13d33
joe.a.burmeister@gmail.com <joe.a.burmeister@gmail.com>
2023-06-16 21:17:06 +0100

    Fill in more of ESP wifi protocol.

--- ports/esp32/src/protocol.c
+++ ports/esp32/src/protocol.c
@@ -1,17 +1,25 @@
+#include <string.h>
+
+#include <esp_wifi.h>
+#include <esp_event.h>
+
 #include "log.h"
 #include "base_types.h"
 #include "persist_config.h"
 
 #include "protocol.h"
 
+static volatile bool _has_ip_addr = false;
+
 typedef struct
 {
     char ssid[32];
     char password[32];
-} wifi_config_t;
+    wifi_auth_mode_t automode;
+} osm_wifi_config_t;
 
 
-static wifi_config_t* _wifi_get_config(void)
+static osm_wifi_config_t* _wifi_get_config(void)
 {
     comms_config_t* comms_config = &persist_data.model_config.comms_config;
     if (comms_config->type != COMMS_TYPE_LW)
@@ -19,15 +27,96 @@ static wifi_config_t* _wifi_get_config(void)
         comms_debug("Tried to get config for WiFi but config is not for WiFi.");
         return NULL;
     }
-    return (wifi_config_t*)(comms_config->setup);
+    return (osm_wifi_config_t*)(comms_config->setup);
+}
+
+
+static void _wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT)
+    {
+        switch(event_id)
+        {
+            case WIFI_EVENT_STA_START:
+                comms_debug("WiFi STA start event.");
+                esp_wifi_connect();
+                break;
+            case WIFI_EVENT_STA_DISCONNECTED:
+                comms_debug("WiFi STA disconnect event.");
+                _has_ip_addr = false;
+                esp_wifi_connect();
+                break;
+            default:
+                comms_debug("Unknown WiFi STA event :");
+                break;
+        }
+    }
+    else if (event_base == IP_EVENT)
+    {
+        switch(event_id)
+        {
+            case IP_EVENT_STA_GOT_IP:
+                ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+                comms_debug("Got IP:"IPSTR, IP2STR(&event->ip_info.ip));
+                _has_ip_addr = true;
+                break;
+            default:
+                comms_debug("Unknown WiFi IP event :");
+                break;
+        }
+    }
+}
+
+
+static void _start_wifi(void)
+{
+    osm_wifi_config_t* osm_config = _wifi_get_config();
+    if (!osm_config)
+        return;
+    unsigned ssid_len   = strlen(osm_config->ssid);
+    unsigned passwd_len = strlen(osm_config->password);
+
+    if (!ssid_len || !passwd_len)
+        return;
+
+    wifi_config_t wifi_config =
+    {
+        .sta =
+        {
+            .threshold.authmode = osm_config->automode,
+            .pmf_cfg =
+            {
+                .capable = true,
+                .required = false
+            },
+        },
+    };
+
+    memcpy(wifi_config.sta.ssid, osm_config->ssid, ssid_len+1);
+    memcpy(wifi_config.sta.password, osm_config->password, passwd_len+1);
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+
+    ESP_ERROR_CHECK(esp_wifi_start());
 }
 
 
 void protocol_system_init(void)
 {
-    wifi_config_t* wifi_config = _wifi_get_config();
-    if (!wifi_config)
-        return;
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, _wifi_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, _wifi_event_handler, NULL));
+
+    _start_wifi();
 }
 
 
@@ -41,14 +130,19 @@ bool        protocol_send_ready(void) { return false; }
 bool        protocol_send_allowed(void) { return false; }
 void        protocol_reset(void) {}
 void        protocol_process(char* message) {}
+
+
 bool protocol_get_connected(void)
 {
-    return false;
+    return _has_ip_addr;
 }
+
 
 void protocol_loop_iteration(void) {}
 
 bool        protocol_get_id(char* str, uint8_t len) { return false; }
-struct cmd_link_t* protocol_add_commands(struct cmd_link_t* tail) { return tail; }
 
 void        protocol_power_down(void) {}
+
+
+struct cmd_link_t* protocol_add_commands(struct cmd_link_t* tail) { return tail; }
