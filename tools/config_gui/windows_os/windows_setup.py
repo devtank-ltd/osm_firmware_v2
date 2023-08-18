30dc405262d9e2fccc3c20f2f5458f0043403d28
Marcus Holder <marcus.holder@devtank.co.uk>
2023-08-18 12:24:36 +0100

    Add icon for windows GUI and update options for py2exe executable.

--- tools/config_gui/windows_os/windows_setup.py
+++ tools/config_gui/windows_os/windows_setup.py
@@ -1,16 +1,19 @@
 from distutils.core import setup
 import py2exe
+
 #Run this program on a windows operating system to create a config gui executable e.g python windows_setup.py py2exe
 
 # Dependencies (add any additional dependencies here)
 dependencies = [
     "binding",
     "modbus_db",
-    "modbus_funcs"
+    "modbus_funcs",
+    "gui_binding_interface"
 ]
 
 # Data files
 data_files = [
+    ("yaml_files", ["yaml_files/modbus_data.yaml", "yaml_files/del_file.yaml"]),
     ("osm_pictures", ["osm_pictures/icons-together.png",
                       "osm_pictures/OSM+Powered.png",
                       "osm_pictures/Lora-Rev-C.png",
@@ -26,12 +29,18 @@ data_files = [
 # Create the setup configuration
 setup(
     py_modules = [],
-    windows=["config_gui.py"],  # Main script to be executed
+    windows=[{
+                "script": "config_gui.py",   # Main script to be executed
+                "icon_resources": [(1, "osm_pictures/OpenSmartMonitor-Logo.ico")]
+            }],
     options={
         "py2exe": {
             "includes": dependencies,  # Include dependencies
-            "bundle_files": 1  # Bundle files into the executable
+            "bundle_files": 1,  # Bundle files into the executable
+            "compressed": 1,
+            "optimize": 2
         }
     },
+    zipfile=None,
     data_files=data_files,  # Include data files
 )
