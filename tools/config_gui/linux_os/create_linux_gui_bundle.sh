da777f9ed1be1d847db728cbf007203d54fd9b54
Marcus Holder <marcus.holder@devtank.co.uk>
2023-08-21 12:26:06 +0100

    Change name of output file for linux gui bundle.

--- tools/config_gui/linux_os/create_linux_gui_bundle.sh
+++ tools/config_gui/linux_os/create_linux_gui_bundle.sh
@@ -5,7 +5,7 @@ cd "$own_dir"
 
 if [ -z "$(which pyinstaller)" ]; then echo "No pyinstaller installed"; exit 1; fi
 if [ -z "$(which patchelf)" ]; then echo "No patchelf installed"; exit 1; fi
-if [ -z "$(which staticx)" ]; then echo "No static installed"; exit 1; fi
+if [ -z "$(which staticx)" ]; then echo "No staticx installed"; exit 1; fi
 
 if [ ! -e ../stm32flash-*/stm32flash ]
 then
@@ -17,14 +17,18 @@ then
   cp ../stm32flash-*/stm32flash ../stm32flash
 fi
 
-pyinstaller --onefile ../config_gui.py --hidden-import='PIL._tkinter_finder' --distpath .
+pyinstaller --onefile ../config_gui.py -n OSM\ Configuration\ GUI --hidden-import='PIL._tkinter_finder' --distpath .
 
-staticx config_gui config_gui
+staticx OSM\ Configuration\ GUI OSM\ Configuration\ GUI
 
-mkdir -p /tmp/osm_gui_bundle
+mkdir -p osm_gui_bundle
 
-tar -czvf /tmp/osm_gui_bundle/osm_gui_bundle.tar.gz config_gui ../config_database/ ../firmware_imgs/complete.bin ../stm32flash ../yaml_files/ ../osm_pictures/ ../docs/config_gui_manual.pdf ../README.md
+cp -r OSM\ Configuration\ GUI ../config_database/ ../../../build/env01c/complete.bin ../stm32flash ../yaml_files/ ../osm_pictures/ ../docs/config_gui_manual.pdf osm_gui_bundle
 
-echo "=================================================="
-echo "Bundle : /tmp/osm_gui_bundle/osm_gui_bundle.tar.gz"
-echo "=================================================="
+tar -czvf /tmp/osm_gui_bundle.tar.gz osm_gui_bundle
+
+rm -r OSM\ Configuration\ GUI OSM\ Configuration\ GUI.spec osm_gui_bundle build
+
+echo "==================================="
+echo "Bundle : /tmp/osm_gui_bundle.tar.gz"
+echo "==================================="
