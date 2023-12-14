876f933889cbeb311fb58b533d337c47b2ada5a6
Marcus Holder <marcus.holder@devtank.co.uk>
2023-12-14 12:20:46 +0000

    Restructure app so that html file is at the top level.

--- tools/config_gui_web/app/modules/gui/navbar.js
+++ tools/config_gui_web/app/modules/gui/navbar.js
@@ -3,7 +3,7 @@ const navbar = `
     <a id="home">Home</a>
     <a id="console">Console</a>
     <a id="modbus">Modbus</a>
-  <img id="nav-img" src="../imgs/osm.png" alt="OpenSmartMonitor Navbar Logo">
+  <img id="nav-img" src="./imgs/osm.png" alt="OpenSmartMonitor Navbar Logo">
 </div>
 `
 
