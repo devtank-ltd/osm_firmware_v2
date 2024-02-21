f8ff4fac3f6bac652e9a46968587334a12f61365
Harry Geyer <harry.geyer@devtank.co.uk>
2024-02-21 16:54:25 +0000

    Included the libopencm3 headers for defines in the model pinmap.

--- model/env01c/model_pinmap.h
+++ model/env01c/model_pinmap.h
@@ -1,6 +1,13 @@
 #pragma once
 
 
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/dma.h>
+#include <libopencm3/cm3/nvic.h>
+
+
 #define ADCS_PORT_N_PINS                            \
 {                                                   \
     {GPIOA, GPIO1},      /* ADC 1  = Channel 6  */  \
