64c4c3cd038c7976aec5e5ceba4c8a10dfc59806
Harry Geyer <harry.geyer@devtank.co.uk>
2024-03-27 13:37:44 +0000

    Initialise and setup RESET as input.

--- comms/src/rak3172.c
+++ comms/src/rak3172.c
@@ -413,6 +413,11 @@ void rak3172_init(void)
     {
         comms_debug("Config is incorrect, not initialising.");
     }
+    rcc_periph_clock_enable(PORT_TO_RCC(_rak3172_ctx.reset_pin.port));
+    gpio_mode_setup(_rak3172_ctx.reset_pin.port,
+                    GPIO_MODE_INPUT,
+                    GPIO_PUPD_NONE,
+                    _rak3172_ctx.reset_pin.pins);
     _rak3172_ctx.state = RAK3172_STATE_OFF;
 }
 
