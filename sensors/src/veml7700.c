60f3bebe634decc2f309b611f47ec1d090b29035
Harry Geyer <harry.geyer@devtank.co.uk>
2024-05-08 10:08:39 +0100

    Increased the collect time for VEML7700 (light sensor).

--- sensors/src/veml7700.c
+++ sensors/src/veml7700.c
@@ -28,7 +28,7 @@ Application Guide:
 #define VEML7700_COLLECTION_TIME_OFFSET_MS      1000
 #define VEML7700_RES_SCALE                      10000
 #define VEML7700_COUNT_LOWER_THRESHOLD          100
-#define VEML7700_DEFAULT_COLLECT_TIME           6600  // Max time in ms is 6.6, so collect + offset
+#define VEML7700_DEFAULT_COLLECT_TIME           13200  // Max time in ms is 13.2, so collect + offset
 #define VEML7700_MAX_READ_TIME                  VEML7700_DEFAULT_COLLECT_TIME + 100
 #define VEML7700_TIMEOUT_TIME_MS                1000
 
