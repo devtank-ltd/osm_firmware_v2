5b783b6fe42b233d8aeb2727009f288338b6175d
Harry Geyer <harry.geyer@devtank.co.uk>
2024-05-10 16:55:57 +0100

    Start moving to OOP model for updating wifi

--- python/wifi_fw_download.py
+++ python/wifi_fw_download.py
@@ -16,135 +16,146 @@ import paho.mqtt.client as mqtt
 protocol_version = b'\x01'
 
 logging.basicConfig(level=logging.INFO, datefmt="%Y-%m-%dT%H:%M:%S", format="[%(asctime)s.%(msecs)03d] %(filename)s:%(funcName)s [%(levelname)s]: %(message)s")
-_logger = logging.getLogger(__name__)
 
 
-MTU = 32
+
+class wifi_fw_dl_t(object):
+    MTU                 = 90
+    COMMAND_RESP_NONE   = 0x00
+    COMMAND_RESP_OK     = 0x01
+    COMMAND_RESP_ERR    = 0x02
+
+    def __init__(self, address, port, firmware_file, hwid, user=None, password=None, ca=None, websockets=False, use_ssl=False, insecure=False, verbose=False):
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        self._logger = logging.getLogger(__name__)
+        self.client.enable_logger(self._logger)
+        if verbose:
+            self._logger.info("ENABLED DEBUG")
+            self._logger.setLevel(logging.DEBUG)
+
+        self.client.on_connect = self._on_connect
+        self.client.on_disconnect = self._on_disconnect
+        self.client.on_subscribe = self._on_subscribe
+        self.client.on_message = self._on_message
 
 
-COMMAND_RESP_NONE   = 0x00
-COMMAND_RESP_OK     = 0x01
-COMMAND_RESP_ERR    = 0x02
+        if len(hwid) > 2 and hwid[:2].lower() == "0x":
+            hwid = hwid[2:]
+        if len(hwid) < 8:
+            hwid = "0" * (8 - len(hwid)) + hwid
+        self.hwid = hwid
 
+        sub_topic = f"osm/{hwid}/cmd/resp"
+        pub_topic = f"osm/{hwid}/cmd"
 
-def _make_payload(cmd):
-    return json.dumps({"CCMD": cmd})
+        firmware = firmware_file.read()
 
+        chunks = [ firmware[int(self.MTU * i / 2): int(self.MTU * (i + 1) / 2)].hex().rjust(self.MTU, "0") for i in range(int(len(firmware)/2)) ]
 
-def _send_packet(client, userdata):
-    pos = userdata.get("pos", 0)
+        self.commands = ["meas_enable 0"]
+        self.commands += [f"fw+ {chunk}" for chunk in chunks]
+        self.commands += [f"fw@"]
+        self.command_index = 0
 
-    firmware = userdata["firmware"]
+        self.client.user_data_set({
+            "sub_topic": sub_topic,
+            "pub_topic": pub_topic,
+            "firmware": firmware,
+            "pos": 0,
+            "crc": CrcModbus.calc(firmware),
+            "mtu": 32,
+        })
 
-    if pos == len(firmware):
-        # FINISH TRANSMISSION
-        _finish_fw(client, userdata)
-        payload = _make_payload()
-        client.publish(userdata["pub_topic"], "fw@")
-        _logger.info("FINISHED TRANSMISSION")
-        done = False
-        return
+        if websockets:
+            self.client.transport = "websockets"
+            self._logger.debug("USING WEBSOCKETS")
 
-    chunk = firmware[pos : pos + MTU].hex()
-    if len(chunk) % 2:
-        chunk = "0" + chunk
-    client.publish(userdata["pub_topic"], f"fw+ {chunk}")
-    userdata.update({"pos": pos + MTU })
-    client.user_data_set(userdata)
+        if use_ssl:
+            if insecure:
+                self._logger.debug("USING SSL NO VERIFY")
+                self.client.tls_set(cert_reqs=ssl.CERT_NONE)
+                self.client.tls_insecure_set(True)
+            else:
+                self._logger.debug(f"USING SSL WITH CA: {ca}")
+                self.client.tls_set(ca_certs=ca)
 
-def _on_connect(client, userdata, flags, reason_code, properties):
-    if reason_code ==0:
-        _logger.info("CONNECTED")
-        _logger.info(f"SUBSCRIBING TO '{userdata['sub_topic']}'")
-        client.subscribe(userdata["sub_topic"])
-    if reason_code > 0:
-        _logger.error(f"FAILED TO CONNECT: '{reason_code}'")
+        if user:
+            self._logger.debug(f"USING AUTHORISATION FOR '{user}'")
+            self.client.username_pw_set(user, password=password)
 
-def _on_disconnect(client, userdata, disconnect_flags, reason_code, properties):
-    _logger.info(f"DISCONNECTED: '{reason_code}'")
+        self._logger.info("STARTING CONNECT")
+        self.client.connect(address, port)
+        self.done = False
 
-def _on_subscribe(client, userdata, mid, reason_code_list, properties):
-    for reason_code in reason_code_list:
-        if reason_code < 128:
-            _logger.info("SUBSCRIBED")
-            # SENDING START / KICKOFF
-            _send_packet(client, userdata)
-        else:
-            _logger.error(f"FAILED TO SUBSCRIBE: '{reason_code}'")
+    def close(self):
+        if self.client:
+            self.client.disconnect()
+            self.client = None
 
-def _on_message(client, userdata, message):
-    _logger.info(f"RECEIVED MESSAGE << '{message.topic}':{message.payload}")
-    data = json.loads(message.payload.decode())
-    print(data)
-    ret_code_str = data.get("ret_code")
-    if ret_code_str:
-        ret_code = int(ret_code_str[2:], 16)
-        print(f"{ret_code = }")
-        print(f"{type(ret_code) = }")
-        if ret_code == COMMAND_RESP_OK:
-            _send_packet(client, userdata)
-        else:
-            time.sleep(1)
-            _logger.error(f"UNEXPECTED RETURN CODE: {ret_code}")
-            done = True
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def __del__(self):
+        self.close()
+
+    def _make_payload(self, cmd):
+        return json.dumps({"CCMD": cmd})
+
+    def _send_packet(self, client, userdata):
+        if self.command_index >= len(self.commands):
+            self._logger.info("NO MORE TO SEND")
+            self.done = True
+            return
+        client.publish(userdata["pub_topic"], self.commands[self.command_index])
+        self.command_index += 1
+
+    def _on_connect(self, client, userdata, flags, reason_code, properties):
+        if reason_code ==0:
+            self._logger.info("CONNECTED")
+            self._logger.info(f"SUBSCRIBING TO '{userdata['sub_topic']}'")
+            client.subscribe(userdata["sub_topic"])
+        if reason_code > 0:
+            self._logger.error(f"FAILED TO CONNECT: '{reason_code}'")
+
+    def _on_disconnect(self, client, userdata, disconnect_flags, reason_code, properties):
+        self._logger.info(f"DISCONNECTED: '{reason_code}'")
+
+    def _on_subscribe(self, client, userdata, mid, reason_code_list, properties):
+        for reason_code in reason_code_list:
+            if reason_code < 128:
+                self._logger.info("SUBSCRIBED")
+                # SENDING START / KICKOFF
+                self._send_packet(client, userdata)
+            else:
+                self._logger.error(f"FAILED TO SUBSCRIBE: '{reason_code}'")
+
+    def _on_message(self, client, userdata, message):
+        self._logger.info(f"RECEIVED MESSAGE << '{message.topic}':{message.payload}")
+        data = json.loads(message.payload.decode())
+        print(data)
+        ret_code_str = data.get("ret_code")
+        if ret_code_str:
+            ret_code = int(ret_code_str[2:], 16)
+            print(f"{ret_code = }")
+            print(f"{type(ret_code) = }")
+            if ret_code == self.COMMAND_RESP_OK:
+                time.sleep(0.3)
+                self._send_packet(client, userdata)
+            else:
+                self._logger.error(f"UNEXPECTED RETURN CODE: {ret_code}")
+                self.done = True
+
+    def run(self):
+        while not self.done:
+            self.client.loop()
 
 
 def main(args):
-    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-
-    client.enable_logger(_logger)
-    if args.verbose:
-        _logger.info("ENABLED DEBUG")
-        _logger.setLevel(logging.DEBUG)
-
-    client.on_connect = _on_connect
-    client.on_disconnect = _on_disconnect
-    client.on_subscribe = _on_subscribe
-    client.on_message = _on_message
-
-    hwid = args.hwid
-    if len(hwid) > 2 and hwid[:2].lower() == "0x":
-        hwid = hwid[2:]
-    if len(hwid) < 8:
-        hwid = "0" * (8 - len(hwid)) + hwid
-    sub_topic = f"osm/{hwid}/cmd/resp"
-    pub_topic = f"osm/{hwid}/cmd"
-
-    firmware = args.firmware.read()
-
-    client.user_data_set({
-        "sub_topic": sub_topic,
-        "pub_topic": pub_topic,
-        "firmware": firmware,
-        "pos": 0,
-        "crc": CrcModbus.calc(firmware),
-        "mtu": 32,
-    })
-
-    if args.websockets:
-        client.transport = "websockets"
-        _logger.debug("USING WEBSOCKETS")
-
-    if args.ssl:
-        if args.insecure:
-            _logger.debug("USING SSL NO VERIFY")
-            client.tls_set(cert_reqs=ssl.CERT_NONE)
-            client.tls_insecure_set(True)
-        else:
-            _logger.debug(f"USING SSL WITH CA: {args.ca}")
-            client.tls_set(ca_certs=args.ca)
-
-    if args.user:
-        _logger.debug(f"USING AUTHORISATION FOR '{args.user}'")
-        client.username_pw_set(args.user, password=args.password)
-
-    _logger.info("STARTING CONNECT")
-    client.connect(args.address, args.port)
-
-    global done
-    done = False
-    while not done:
-        client.loop()
+    with wifi_fw_dl_t(args.address, args.port, args.firmware, args.hwid, user=args.user, password=args.password, ca=args.ca, websockets=args.websockets, use_ssl=args.ssl, insecure=args.insecure, verbose=args.verbose) as wifi_fw_dl:
+        wifi_fw_dl.run()
     return 0
 
 
