fbbed233ee3d52d67c253ff2db519b465d32c9ab
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-07-22 17:27:37 +0100

    Improve the flash scripts for when used in standalone release bundle.

--- tools/config_scripts/config_load.sh
+++ tools/config_scripts/config_load.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+if [ -z "$1" ]
+then
+  echo "Please provide config file to load on to OSM."
+  exit -1
+fi
+
 . "$(dirname "$0")"/boot_mode.sh
 
 stm32flash -b 115200 -i $gpio_str -S ${config_addr}:${config_size} -w $1 /dev/$dev
