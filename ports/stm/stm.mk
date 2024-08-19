06585f1cc8c975cff40df29eaad2b7d70755ed63
Harry Geyer <harry.geyer@devtank.co.uk>
2024-08-19 15:25:25 +0100

    Added json config tool (and binding for this) to the release bundle.

--- ports/stm/stm.mk
+++ ports/stm/stm.mk
@@ -118,6 +118,8 @@ $$(RELEASE_DIR)/$(1)_$$(RELEASE_NAME).tar.gz : $$(BUILD_DIR)/$(1)/complete.bin
 	  mkdir -p $$(RELEASE_DIR)/$(1)_$$(RELEASE_NAME); \
 	  cp -r $$(BUILD_DIR)/$(1)/complete.bin $$(RELEASE_DIR)/$(1)_$$(RELEASE_NAME)/$(1)_$$(RELEASE_NAME).bin; \
 	  cp -r $$(OSM_DIR)/tools/config_scripts/* $$(RELEASE_DIR)/$(1)_$$(RELEASE_NAME)/; \
+	  cp -r $$(OSM_DIR)/tools/json_config_tool/json_config.py $$(RELEASE_DIR)/$(1)_$$(RELEASE_NAME)/; \
+	  cp -r $$(OSM_DIR)/python/binding.py $$(RELEASE_DIR)/$(1)_$$(RELEASE_NAME)/; \
 	  cd $$(RELEASE_DIR); \
 	  tar -czf $(1)_$$(RELEASE_NAME).tar.gz $(1)_$$(RELEASE_NAME); \
 	  rm -r $(1)_$$(RELEASE_NAME); \
