1c77457c7bcad52c0e93a0c3261c154524b13eb9
Harry Geyer <harry.geyer@devtank.co.uk>
2025-01-15 12:18:06 +0000

    Moved timer command to timers file.
    Conflict resolved during rebase by: m3g4d1v3r <99227953+m3g4d1v3r@users.noreply.github.com>

--- src/ports/stm/timers.c
+++ src/ports/stm/timers.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include <libopencm3/stm32/timer.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
@@ -6,6 +8,8 @@
 #include <osm/core/config.h>
 #include <osm/core/timers.h>
 #include <osm/core/log.h>
+#include <osm/core/base_types.h>
+#include <osm/core/common.h>
 
 
 static volatile uint32_t us_counter = 0;
@@ -47,4 +51,25 @@ void     osm_timers_init()
     timer_set_period(TIM2, 0xffff);
     timer_enable_preload(TIM2);
     timer_one_shot_mode(TIM2);
+}
+
+
+static osm_command_response_t _cmd_timer_cb(char* args, osm_cmd_ctx_t * ctx)
+{
+    char* pos = osm_skip_space(args);
+    uint32_t delay_ms = strtoul(pos, NULL, 10);
+    uint32_t start_time = osm_get_since_boot_ms();
+    osm_timer_delay_us_64(delay_ms * 1000);
+    osm_cmd_ctx_out(ctx,"Time elapsed: %"PRIu32, osm_since_boot_delta(osm_get_since_boot_ms(), start_time));
+    return OSM_COMMAND_RESP_OK;
+}
+
+
+struct osm_cmd_link_t* timers_add_commands(struct osm_cmd_link_t* tail)
+{
+    static struct osm_cmd_link_t cmds[] =
+    {
+        { "timer",        "Test usecs timer",         _cmd_timer_cb                  , false , NULL},
+    };
+    return osm_add_commands(tail, cmds, OSM_ARRAY_SIZE(cmds));
 }
