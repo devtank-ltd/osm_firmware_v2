b6f7f4975f35cf1e6ed2023b3c8ae1a2a0657229
Harry Geyer <harry.geyer@devtank.co.uk>
2025-02-05 11:30:39 +0000

    Update upgrade.py

--- tools/upgrade.py
+++ tools/upgrade.py
@@ -1,10 +1,13 @@
 #! /usr/bin/python3
 import os
 import sys
-from binding import io_board_py_t
 
-dev = io_board_py_t(sys.argv[1])
+sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir, "python"))
+
+from binding import dev_t
+
+dev = dev_t(sys.argv[1])
 if "DEBUG" in os.environ:
-    dev.command(b"debug 0x800")
+    dev.set_dbg("0x800")
 dev.fw_upload(sys.argv[2])
 dev.reset()
