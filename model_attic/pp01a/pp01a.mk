34317a9f822eef94217b0ee37014ac5cf5a21651
Harry Geyer <harry.geyer@devtank.co.uk>
2025-02-05 13:39:35 +0000

    Move more of the cmake to port specific, instead of model specific.

--- model_attic/pp01a/pp01a.mk
+++ model_attic/pp01a/pp01a.mk
@@ -1,1 +1,24 @@
+pp01a_SOURCES := \
+    $(OSM_DIR)/src/core/main.c \
+    $(OSM_DIR)/src/core/base.c \
+    $(OSM_DIR)/src/core/cmd.c \
+    $(OSM_DIR)/src/core/common.c \
+    $(OSM_DIR)/src/core/log.c \
+    $(OSM_DIR)/src/core/measurements.c \
+    $(OSM_DIR)/src/core/measurements_mem.c \
+    $(OSM_DIR)/src/core/persist_base.c \
+    $(OSM_DIR)/src/core/platform_common.c \
+    $(OSM_DIR)/src/core/ring.c \
+    $(OSM_DIR)/src/core/uart_rings.c \
+    $(OSM_DIR)/src/ports/rp2350/rp2350.c \
+    $(OSM_DIR)/src/ports/rp2350/persist_config.c \
+    $(OSM_DIR)/src/ports/rp2350/sleep.c \
+    $(OSM_DIR)/src/ports/rp2350/uarts.c \
+    $(OSM_DIR)/src/ports/rp2350/rp2350_comms.c \
+    $(OSM_DIR)/src/ports/rp2350/update.c \
+    $(OSM_DIR)/src/protocols/jsonblob.c \
+    $(OSM_DIR)/src/protocols/comms_behind.c \
+    $(OSM_DIR)/src/sensors/fw.c \
+    $(OSM_MODEL_DIR)/pp01a/main/model.c
+
 $(eval $(call RP2350_FIRMWARE,pp01a))
--- src/ports/rp2350/rp2350.mk
+++ src/ports/rp2350/rp2350.mk
@@ -6,11 +6,11 @@ $(1): $$(OSM_BUILD_DIR)/$(1)/.complete
 ifneq ($$(origin PICO_SDK_PATH), undefined)
 $$(OSM_BUILD_DIR)/$(1)/Makefile:
 	mkdir -p $$(OSM_BUILD_DIR)/$(1)
-	OSM_DIR="$(shell realpath $(OSM_DIR))" OSM_MODEL_DIR="$(shell realpath $(OSM_MODEL_DIR))" OSM_GIT_VERSION="$(OSM_GIT_VERSION)" OSM_GIT_SHA1="$(OSM_GIT_SHA1)" cmake $$(OSM_MODEL_DIR)/$(1)/ -B $$(OSM_BUILD_DIR)/$(1)/
+	OSM_DIR="$$(shell realpath $$(OSM_DIR))" OSM_MODEL_DIR="$$(shell realpath $$(OSM_MODEL_DIR))" OSM_GIT_VERSION="$$(OSM_GIT_VERSION)" OSM_GIT_SHA1="$$(OSM_GIT_SHA1)" OSM_SRCS="$$($(1)_SOURCES)" cmake $$(OSM_MODEL_DIR)/$(1)/ -B $$(OSM_BUILD_DIR)/$(1)/
 
 
 $$(OSM_BUILD_DIR)/$(1)/$(1).bin: $$(OSM_BUILD_DIR)/$(1)/Makefile
-	OSM_DIR="$(shell realpath $(OSM_DIR))" OSM_MODEL_DIR="$(shell realpath $(OSM_MODEL_DIR))" OSM_GIT_VERSION="$(OSM_GIT_VERSION)" OSM_GIT_SHA1="$(OSM_GIT_SHA1)" make -C $$(OSM_BUILD_DIR)/$(1)
+	OSM_DIR="$$(shell realpath $(OSM_DIR))" OSM_MODEL_DIR="$(shell realpath $(OSM_MODEL_DIR))" OSM_GIT_VERSION="$(OSM_GIT_VERSION)" OSM_GIT_SHA1="$(OSM_GIT_SHA1)" OSM_SRCS="$$($(1)_SOURCES)" make -C $$(OSM_BUILD_DIR)/$(1)
 
 $$(OSM_BUILD_DIR)/$(1)/.complete: $$(OSM_BUILD_DIR)/$(1)/$(1).bin
 	mkdir -p $$(OSM_BUILD_DIR)/$(1)
